a4ac9a0f7b3274abe1d40e684fa16865008e4867
Olivier Boudeville <olivier.boudeville@esperide.com>
2006-06-24 18:17:28 +0000

    sed is /usr/bin/sed for NetBSD.

--- protectSpecialCharacters.sh
+++ protectSpecialCharacters.sh
@@ -2,8 +2,12 @@
 
 USAGE="Usage : protectSpecialCharacters.sh EXPR\n	prevent special characters  in EXPR  from being interpreted by tools like sed \n example : \n\tprotectSpecialCharacters.sh eee/dd/f \n\tshould output eee\/dd\/f"
 
-SED=/bin/sed
-			 
+SED=/usr/bin/sed
+if [ ! -x "${SED}" ] ; then
+	SED=/usr/bin/sed
+fi
+
+
 if [ $# != 1 ]; then
 	echo -e $USAGE
 	exit 1
