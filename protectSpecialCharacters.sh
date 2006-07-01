4427f4961943227ca3f7340f23bcc822fb10f4aa
Olivier Boudeville <olivier.boudeville@esperide.com>
2006-07-01 08:59:19 +0000

    SED path fixed.

--- protectSpecialCharacters.sh
+++ protectSpecialCharacters.sh
@@ -2,7 +2,7 @@
 
 USAGE="Usage : protectSpecialCharacters.sh EXPR\n	prevent special characters  in EXPR  from being interpreted by tools like sed \n example : \n\tprotectSpecialCharacters.sh eee/dd/f \n\tshould output eee\/dd\/f"
 
-SED=/usr/bin/sed
+SED=/bin/sed
 if [ ! -x "${SED}" ] ; then
 	SED=/usr/bin/sed
 fi
