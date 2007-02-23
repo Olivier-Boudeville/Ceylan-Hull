b9ee0446949050bee37b708769288ebd9b970eaa
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-02-23 10:11:02 +0000

    _exe was lacking.

--- generateTestRules.sh
+++ generateTestRules.sh
@@ -5,7 +5,7 @@ USAGE=`basename $0`" : generates automake-specific rules to specify the sources 
 
 for t in test*.cc; do
 	program_name=`echo $t | sed 's|.cc$||1'`
-	echo "${program_name}_SOURCES = $t"
+	echo "${program_name}_exe_SOURCES = $t"
 done
 
 # Sorted alphabetically, but no formatting done to respect columns.
