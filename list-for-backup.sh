2bc5c396188da7e2e3024f6769d27d9500971359
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-02-23 13:26:57 +0000

    Useful for DVD backups.

---
+++ list-for-backup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+LISTING_FILE="$HOME/"`date "+%Y%m%d"`"-archive-listing.txt"
+
+USAGE="Enumerates in current directory all files, specifies their name, size and MD5 sum, and stores the result in $LISTING_FILE."
+
+echo $USAGE
+
+echo "
+
+	Listing archive content : 
+	
+
+" > $LISTING_FILE
+
+for f in * ; do
+
+	MD5=`md5sum $f | awk '{ print $1 }'`
+	SIZE=`du -sh $f | awk '{ print $1 }'`
+	
+	printf  "%6s  %32s  %s\n" $SIZE $MD5 $f >> $LISTING_FILE
+	
+done
+
+echo "Listing file generated in $LISTING_FILE." 
+
