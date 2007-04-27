9119ec04c11834ca28039371406302a4287a4166
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-04-27 11:43:00 +0000

    Updated with better comments, tests, replacements, etc.

--- correctFilename.sh
+++ correctFilename.sh
@@ -4,23 +4,32 @@ SED=`which sed | grep -v ridiculously`
 MV=`which mv | grep -v ridiculously`
 
 USAGE="
-Usage : "`basename $0`" <a file name> : renames the specified file with a 'corrected' filename, i.e. without space, replaced by '-', nor accentuated characters in it."
+Usage : "`basename $0`" <a directory entry name> : renames the specified file or directory to a 'corrected' filename, i.e. without space, replaced by '-', nor accentuated characters in it."
 
-if [ $# == "0" ]; then
-	echo -e "Error, no argument given. $USAGE" 1>&2
+if [ $# -eq 0 ] ; then
+	echo "
+	
+	Error, no argument given. $USAGE
+	
+	" 1>&2
 	exit 1
 fi
 
+
 ORIGINAL_NAME="$*"
 
-if [ ! -f "${ORIGINAL_NAME}" ]; then
-	echo -e "Error, no file named <${ORIGINAL_NAME}> exists. $USAGE" 1>&2
+if [ ! -e "${ORIGINAL_NAME}" ] ; then
+	echo "
+	
+	Error, no entry named <${ORIGINAL_NAME}> exists. $USAGE
+	
+	" 1>&2
 	exit 2
 fi
 	
 #echo "Original name is : <${ORIGINAL_NAME}>"
 
-CORRECTED_NAME=`echo ${ORIGINAL_NAME} | ${SED} 's| |-|g' | ${SED} 's|é|e|g' | ${SED} 's|è|e|g' | ${SED} 's|ê|e|g' | ${SED} 's|à|a|g' | ${SED} 's|â|a|g'| ${SED} 's|î|i|g'| ${SED} 's|û|u|g'| ${SED} 's|ô|o|g'`
+CORRECTED_NAME=`echo ${ORIGINAL_NAME} | ${SED} 's| |-|g' | ${SED} 's|--|-|g' | ${SED} 's|é|e|g' | ${SED} 's|è|e|g' | ${SED} 's|ê|e|g' | ${SED} 's|à|a|g' | ${SED} 's|â|a|g'| ${SED} 's|î|i|g'| ${SED} 's|û|u|g'| ${SED} 's|ô|o|g'`
 
 
 #echo "Corrected name is : <${CORRECTED_NAME}>"
@@ -29,7 +38,10 @@ CORRECTED_NAME=`echo ${ORIGINAL_NAME} | ${SED} 's| |-|g' | ${SED} 's|é|e|g' | ${
 
 if [ "${ORIGINAL_NAME}" != "${CORRECTED_NAME}" ]; then
 	if [ -f "${CORRECTED_NAME}" ]; then
-		echo -e "Error, a file named <${CORRECTED_NAME}> already exists, corrected filename for <${ORIGINAL_NAME}> collides with it, remove <${CORRECTED_NAME}> first." 1>&2
+		echo "
+		
+		Error, an entry named <${CORRECTED_NAME}> already exists, corrected name for <${ORIGINAL_NAME}> collides with it, remove <${CORRECTED_NAME}> first.
+		" 1>&2
 		exit 3
 	fi
 
