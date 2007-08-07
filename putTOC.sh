701d773e985964af37628ddc47ef7de5b4ee7efa
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-08-07 09:39:34 +0000

    Updated, more convenient now.

--- putTOC.sh
+++ putTOC.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
 echo 
-echo "<a name=\"maintoc\"></a>"
+echo "<a name=\"XXXtoc\"></a>"
 echo "<center>"
-echo "	<table summary=\"Table of contents\" style=\"border: none\">"
+echo "	<table summary=\"Table of contents for XXX\" style=\"border: none\">"
 echo "        <tr>"
 echo "          <td>"
-echo "<!--"
-echo "		       <a href=\"#\"></a><br>"
-echo "-->"
-echo		  
 echo "		   <div class=\"box\">"
 echo "		       <a href=\"#\"></a><br>"
 echo "		       <a href=\"#\"></a><br>"
@@ -19,13 +15,20 @@ echo "		       <a href=\"#\"></a><br>"
 echo "		       <a href=\"#\"></a><br>"
 echo "		       <a href=\"#\"></a><br>"
 echo "		       <a href=\"#\"></a>"
+echo		  
+echo "<!--"
+echo "		       <a href=\"#\"></a><br>"
+echo "-->"
+echo		  
 echo "          </div>"
 echo 
 echo "          </td>"
 echo "        </tr>"
 echo "      </table>"
+echo "[<a href=\"#maintoc\">Back to the main table of contents</a>]"
 echo "</center>"
+echo 
 echo "<!--"
-echo "<p><center>[<a href=\"#maintoc\">Back to the table of contents</a>]</center></p><br><br>"
+echo "<p><center>[<a href=\"#XXXtoc\">Back to the table of contents</a>]</center></p><br><br>"
 echo "-->"
 echo 
