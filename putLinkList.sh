bc3fee218ea24f9c9772bc00a6899db34d79eb4a
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-08-07 09:40:01 +0000

    Reordered.

--- putLinkList.sh
+++ putLinkList.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 echo "<ul>"
+echo "    <li><a href=\"\" target=\"_blank\"></a></li>"
 echo
 echo "<!--"
 echo "    <li><a href=\"\" target=\"_blank\"></a></li>"
 echo "-->"
 echo 
-echo "    <li><a href=\"\" target=\"_blank\"></a></li>"
 echo "</ul>"
