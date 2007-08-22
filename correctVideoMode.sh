0c4ff43c26b798cf698eb9e0e0ef3830de83c4b4
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-08-22 07:58:53 +0000

    Updates the resolution, as xorg.conf does not seem able to do so by itself.

---
+++ correctVideoMode.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+XRANDR=/usr/bin/xrandr
+
+# If 1440x900, set 1680x1050:
+
+CURRENT_RES=`${XRANDR} | grep '*' | awk '{print $2 $4}'`
+
+TARGET_RES="1680x1050"
+
+#echo "Current resolution is ${CURRENT_RES}"
+
+if [ "${CURRENT_RES}" = "1440900" ] ; then
+	echo "Switching to ${TARGET_RES}..."
+	${XRANDR} --size ${TARGET_RES}
+	echo "...done"
+else
+	echo "(nothing done, already at target resolution ${TARGET_RES})"
+fi
