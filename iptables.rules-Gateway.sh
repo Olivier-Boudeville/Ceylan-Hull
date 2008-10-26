242ae7f1d4fe43df6d4fdc5d934cfbb7c0f9d529
Olivier Boudeville <olivier.boudeville@esperide.com>
2008-10-26 21:34:21 +0000

    Slight reformatting.

--- iptables.rules-Gateway.sh
+++ iptables.rules-Gateway.sh
@@ -10,14 +10,14 @@
 # Adapted from GNU Linux Magazine France, number 83 (may 2006), p.14
 # (article written by Christophe Grenier, grenier@cgsecurity.org)
 
-# To debug this kind of firewall script, one may use :
+# To debug this kind of firewall script, one may use:
 # sh -x /path/to/this/file
 
 # Useful with 'iptables --list|grep '[v' or 'iptables -L -n |grep [v'
-# to check whether rules are up-to-date :
-version=5
+# to check whether rules are up-to-date:
+version=6
 
-# Full path of the programs we need, change them to your needs :
+# Full path of the programs we need, change them to your needs:
 iptables=/sbin/iptables
 modprobe=/sbin/modprobe
 echo=/bin/echo
@@ -30,28 +30,28 @@ $echo "Setting ADSL Gateway firewall rules, version $version"
 touch $LOG_FILE 
 
 # Only needed for older distros that do load ipchains by default, 
-# just unload it :
+# just unload it:
 if $lsmod  2>/dev/null | grep -q ipchains ; then 
 	$rmmod ipchains
 fi 
 
-# Local (LAN) interface :
+# Local (LAN) interface:
 LAN_IF=eth0
 
 
-# Internet interface :
+# Internet interface:
 
-# For PPP ADSL connection :
+# For PPP ADSL connection:
 #NET_IF=ppp0
 
-# For direct connection to a set-top box from your provider :
+# For direct connection to a set-top box from your provider:
 NET_IF=eth1
 
 
-# Load appropriate modules : 
+# Load appropriate modules: 
 $modprobe ip_tables
 
-# We load these modules as we want to do stateful firewalling :
+# We load these modules as we want to do stateful firewalling:
 $modprobe ip_conntrack 	
 $modprobe ip_conntrack_ftp
 $modprobe ip_conntrack_irc
@@ -61,7 +61,7 @@ $echo "0" > /proc/sys/net/ipv4/ip_forward
 # These lines are here in case rules are already in place and the script is
 # ever rerun on the fly. 
 # We want to remove all rules and pre-exisiting user defined chains and 
-# zero the counters before we implement new rules : 
+# zero the counters before we implement new rules: 
 ${iptables} -F 
 ${iptables} -X 
 ${iptables} -Z 
@@ -87,25 +87,25 @@ ${iptables} -P OUTPUT DROP
 # CONFIG_SYSCTL defined in your kernel.  
 
 # Enable response to ping in the kernel, but we will only answer if 
-# the rule at the bottom of the file let us :
+# the rule at the bottom of the file let us:
 $echo "0" > /proc/sys/net/ipv4/icmp_echo_ignore_all 
 
 # Disable response to broadcasts. 
-# You do not want yourself becoming a Smurf amplifier :
+# You do not want yourself becoming a Smurf amplifier:
 $echo "1" > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts 
 
 # Do not accept source routed packets. 
 # Attackers can use source routing to generate traffic pretending to be from
 # inside your network, but which is routed back along the path from which it
 # came, namely outside, so attackers can compromise your network. 
-# Source routing is rarely used for legitimate purposes :
+# Source routing is rarely used for legitimate purposes:
 $echo "0" > /proc/sys/net/ipv4/conf/all/accept_source_route 
 
 # Disable ICMP redirect acceptance. ICMP redirects can be used to alter your
-# routing tables, possibly to a bad end :
+# routing tables, possibly to a bad end:
 $echo "0" > /proc/sys/net/ipv4/conf/all/accept_redirects 
 
-# Enable bad error message protection :
+# Enable bad error message protection:
 $echo "1" > /proc/sys/net/ipv4/icmp_ignore_bogus_error_responses 
 
 # Turn on reverse path filtering. This helps make sure that packets use 
@@ -117,7 +117,7 @@ $echo "1" > /proc/sys/net/ipv4/icmp_ignore_bogus_error_responses
 # you to a host take a different path than packets from that host to you), 
 # or if you operate a non-routing host which has several IP addresses on
 # different interfaces. 
-# Note  : if you turn on IP forwarding, you will also get this : 
+# Note : if you turn on IP forwarding, you will also get this: 
 for interface in /proc/sys/net/ipv4/conf/*/rp_filter; do 
    $echo "1" > ${interface} 
 done 
@@ -125,7 +125,7 @@ done
 # Log spoofed packets, source routed packets, redirect packets. 
 $echo "1" > /proc/sys/net/ipv4/conf/all/log_martians 
 
-# Make sure that IP forwarding is turned on, as it is a gateway :
+# Make sure that IP forwarding is turned on, as it is a gateway:
 $echo "1" > /proc/sys/net/ipv4/ip_forward 
 
 
@@ -134,19 +134,19 @@ $echo "1" > /proc/sys/net/ipv4/ip_forward
 
 # ----------------  FORWARD ---------------------
 
-## We are masquerading :
+## We are masquerading:
 ${iptables} -t nat -A POSTROUTING -o ${NET_IF} -s 192.168.0.0/24 -j MASQUERADE
 
-# With ADSL connections, NAT reduces the MTU, counter-measure :
+# With ADSL connections, NAT reduces the MTU, counter-measure:
 ${iptables} -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS -o ${NET_IF} --clamp-mss-to-pmtu
 
 # Only forward that stuff from one interface to the other, and do that with
-# connection tracking :
+# connection tracking:
 
-# Everything from the LAN to the Internet is forwarded :
+# Everything from the LAN to the Internet is forwarded:
 ${iptables} -A FORWARD -i ${LAN_IF} -o ${NET_IF} -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 
-# Packets from the Internet to the LAN must not be new unknown connections :
+# Packets from the Internet to the LAN must not be new unknown connections:
 ${iptables} -A FORWARD -o ${LAN_IF} -i ${NET_IF} -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 AD_FILTER_PORT=3129
@@ -158,7 +158,7 @@ AD_FILTER_PORT=3129
 
 # ----------------  OUTPUT ---------------------
 
-# No unroutable (private) adddress should be output by the gateway :
+# No unroutable (private) adddress should be output by the gateway:
 ${iptables} -A OUTPUT -o ${NET_IF} -d 10.0.0.0/8     -j REJECT
 ${iptables} -A OUTPUT -o ${NET_IF} -d 127.0.0.0/8    -j REJECT 
 ${iptables} -A OUTPUT -o ${NET_IF} -d 172.16.0.0/12  -j REJECT 
@@ -166,34 +166,34 @@ ${iptables} -A OUTPUT -o ${NET_IF} -d 192.168.0.0/16 -j REJECT
 
 
 # Second rule is to let packets through which belong to established or 
-# related connections and we let all traffic out, as we trust ourself :
+# related connections and we let all traffic out, as we trust ourself:
 ${iptables} -A OUTPUT -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 
 
 # ----------------  INPUT ---------------------
 
 
-# Log some invalid connections :
-${iptables} -A INPUT -m state --state INVALID -m limit --limit 2/s -j LOG --log-prefix "[v$version : invalid input ] :  "
+# Log some invalid connections:
+${iptables} -A INPUT -m state --state INVALID -m limit --limit 2/s -j LOG --log-prefix "[v$version: invalid input ]:  "
 
 ${iptables} -A INPUT -m state --state INVALID -j DROP
 
-# Filter out broadcasts :
+# Filter out broadcasts:
 ${iptables} -A INPUT -m pkttype --pkt-type broadcast -j DROP
 
 
 # Drops directly connections coming from the Internet with unroutable
-# (private) addresses :
+# (private) addresses:
 ${iptables} -A INPUT -i ${NET_IF} -s 10.0.0.0/8     -j DROP
 ${iptables} -A INPUT -i ${NET_IF} -s 127.0.0.0/8    -j DROP
 ${iptables} -A INPUT -i ${NET_IF} -s 172.16.0.0/12  -j DROP
 ${iptables} -A INPUT -i ${NET_IF} -s 192.168.0.0/16 -j DROP
 
 
-# Avoid stealth TCP port scans if SYN is not set properly :
+# Avoid stealth TCP port scans if SYN is not set properly:
 ${iptables} -A INPUT -m state --state NEW,RELATED -p tcp --tcp-flags ! ALL SYN -j DROP
 
-# Rejects directly 'auth/ident' obsolete requests :
+# Rejects directly 'auth/ident' obsolete requests:
 ${iptables} -A INPUT -p tcp --dport auth -j REJECT --reject-with tcp-reset
 
 ${iptables} -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
@@ -206,27 +206,27 @@ ${iptables} -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 # fragments is very OS-dependent. 
 # We are not going to trust any fragments. 
 # Log fragments just to see if we get any, and deny them too. 
-${iptables} -A INPUT -f -j LOG --log-prefix "[v$version : iptables fragments ] : " 
+${iptables} -A INPUT -f -j LOG --log-prefix "[v$version: iptables fragments ]: " 
 ${iptables} -A INPUT -f -j DROP 
 
-## HTTP (web server) :
+## HTTP (web server):
 ${iptables} -A INPUT -p tcp --dport 80 -m state --state NEW -j ACCEPT
 
-# HTTPS :
+# HTTPS:
 #${iptables} -A INPUT -p tcp --dport 443 -m state --state NEW -j ACCEPT
 
 ## ident, if we drop these packets we may need to wait for the timeouts
 # e.g. on ftp servers
 ${iptables} -A INPUT -p tcp --dport 113 -m state --state NEW -j REJECT
 
-## FTP :
+## FTP:
 ${iptables} -A INPUT -p tcp --dport 20 -m state --state NEW -j ACCEPT
 ${iptables} -A INPUT -p tcp --dport 21 -m state --state NEW -j ACCEPT
 
 
-# Orge section:
+# Orge section:
 
-# For Erlang epmd daemon:
+# For Erlang epmd daemon:
 ${iptables} -A INPUT -p tcp --dport 4369 -m state --state NEW -j ACCEPT
 
 # For the listening socket of TCP Orge server:
@@ -241,28 +241,28 @@ ${iptables} -A INPUT -p tcp --dport 51000:51999 -m state --state NEW,ESTABLISHED
 ${iptables} -A INPUT -p udp --dport 9512 -m state --state NEW -j ACCEPT
 
 
-## SSH :
+## SSH:
 
-# Unlimited logging from LAN :
+# Unlimited logging from LAN:
 ${iptables} -A INPUT -i ${LAN_IF} -p tcp --dport ssh -m state --state NEW -j ACCEPT
 
 # This rules allow to prevent brute-force SSH attacks by limiting the
-# frequency of attempts coming from the Internet :
+# frequency of attempts coming from the Internet:
 
-# Logs too frequent attempts tagged with 'SSH' and drops them :
-${iptables} -A INPUT -i ${NET_IF} -p tcp --dport ssh -m recent --update --seconds 60 --hitcount 4 --name SSH -j LOG --log-prefix "[v$version : SSH brute-force ] : "
+# Logs too frequent attempts tagged with 'SSH' and drops them:
+${iptables} -A INPUT -i ${NET_IF} -p tcp --dport ssh -m recent --update --seconds 60 --hitcount 4 --name SSH -j LOG --log-prefix "[v$version: SSH brute-force ]: "
 
 ${iptables} -A INPUT -i ${NET_IF} -p tcp --dport ssh -m recent --update --seconds 60 --hitcount 4 --name SSH -j DROP
 
-# Tags too frequent SSH attempts with the name 'SSH' :
+# Tags too frequent SSH attempts with the name 'SSH':
 ${iptables} -A INPUT -i ${NET_IF} -p tcp --dport ssh -m recent --set --name SSH
 
-# Accepts nevertheless normal SSH logins :
+# Accepts nevertheless normal SSH logins:
 ${iptables} -A INPUT -i ${NET_IF} -p tcp --dport ssh -j ACCEPT
 
  
 
-## Mail stuff :
+## Mail stuff:
 #${iptables} -A INPUT -p tcp --dport 25  -m state --state NEW -j ACCEPT
 #${iptables} -A INPUT -p tcp --dport 110 -m state --state NEW -j ACCEPT
 #${iptables} -A INPUT -p tcp --dport 143 -m state --state NEW -j ACCEPT
@@ -275,7 +275,7 @@ ${iptables} -A INPUT -i ${LAN_IF} -p tcp --dport 53 -m state --state NEW -j ACCE
 ${iptables} -A INPUT -i ${LAN_IF} -p udp --dport 53 -m state --state NEW -j ACCEPT
 
 
-# NUT, for UPS monitoring :
+# NUT, for UPS monitoring:
 ${iptables} -A INPUT -i ${LAN_IF} -p tcp --dport 3493 -m state --state NEW -j ACCEPT
 ${iptables} -A INPUT -i ${LAN_IF} -p udp --dport 3493 -m state --state NEW -j ACCEPT
 
@@ -287,27 +287,27 @@ ${iptables} -A INPUT -i ${LAN_IF} -p udp --dport 3493 -m state --state NEW -j AC
 #${iptables} -A INPUT -i ${LAN_IF} -p udp --dport 137:139 -m state --state NEW -j ACCEPT
 #${iptables} -A INPUT -i ${LAN_IF} -p tcp --dport 137:139 -m state --state NEW -j ACCEPT
 
-# GnomeMeeting :
+# GnomeMeeting:
 #${iptables} -A INPUT -p tcp --dport 30000:33000 -j ACCEPT
 #${iptables} -A INPUT -p tcp --dport 1720 -j ACCEPT
 #${iptables} -A INPUT -p udp --dport 5000:5006 -j ACCEPT
 
 ## LOOPBACK
 # Allow unlimited traffic on the loopback interface, .
-# e.g. needed for KDE, Gnome, etc. :
+# e.g. needed for KDE, Gnome, etc.:
 ${iptables} -A INPUT  -i lo -j ACCEPT
 ${iptables} -A OUTPUT -o lo -j ACCEPT
 
 
 # ---------------- ICMP ---------------------
 
-# Everybody from the LAN can ping me :
+# Everybody from the LAN can ping me:
 # Remove that line if no one should be able to ping you
 ${iptables} -A INPUT -i ${LAN_IF} -p icmp --icmp-type ping -j ACCEPT
 
 # ---------------- NTP ---------------------
 
-# Local clients can ask for gateway-synchronized time :
+# Local clients can ask for gateway-synchronized time:
 ${iptables} -A INPUT -i ${LAN_IF} -p udp --dport 123 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 
 
