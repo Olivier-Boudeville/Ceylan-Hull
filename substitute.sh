fdebf5b59241bf3fd16cbcdc01fdbf62d4d4cf27
Olivier Boudeville <olivier.boudeville@esperide.com>
2008-12-04 19:41:56 +0000

    Outputs improved.

--- substitute.sh
+++ substitute.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+USAGE="
+  Usage: substitute.sh SOURCE TARGET FILE
+    Replaces in file FILE every symbol SOURCE by symbol TARGET
+	Example:
+		substitute.sh wondrful wonderful myFile.txt
+"
+
 
 CP=/bin/cp
 CAT=/bin/cat
@@ -11,27 +18,29 @@ fi
 	
 RM=/bin/rm
 
-USAGE="Usage : substitute.sh SOURCE TARGET FILE\n	replaces in FILE every symbol SOURCE by symbol TARGET \n example : \n\tsubstitute.sh wondrful wonderful myFile"
 			 
 if [ $# != 3 ]; then
-	echo -e $USAGE
+	echo $USAGE
+	echo "Provided parameters were: $*"
 	exit 1
 fi
 
 
 if [ ! -f $3 ]; then
-	echo -e "Cannot operate on '$3' which is not a regular file"
+	echo "
+	Cannot operate on '$3' which is not a regular file"
 	exit 2
 fi
 
 
 # Currently not used anymore, since using '|' as a sed separator instead of '/'.
  
-# Both scripts should be found in the same directory :
+# Both scripts should be found in the same directory:
 PROTECT_SCRIPT=`dirname $0`/protectSpecialCharacters.sh
 
 if [ ! -x "${PROTECT_SCRIPT}" ]; then
-	echo -e "Cannot find an executable protect script (${PROTECT_SCRIPT}) in "`pwd`
+	echo "
+	Cannot find an executable protect script (${PROTECT_SCRIPT}) in "`pwd`
 	exit 3
 fi	
 
@@ -50,3 +59,4 @@ ${CAT} tempSubstitute | ${SED} -e "s/${SOURCE}/${TARGET}/g" > $3
 #${CAT} tempSubstitute | ${SED} -e "s|${SOURCE}|${TARGET}|g" > $3
 
 ${RM} -f tempSubstitute
+
