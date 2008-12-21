b1ab42a61405ffd8f9496ec9c749dfd0bb4aa9f4
Olivier Boudeville <olivier.boudeville@esperide.com>
2008-12-21 11:46:15 +0000

    Reformatted.

--- rmCVS.sh
+++ rmCVS.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-USAGE="`basename $0` : remove recursively from current directory all CVS-related directories (CVS or cvs) and their content. Use with caution !"
+USAGE="`basename $0`: removes recursively from current directory all CVS-related directories (CVS or cvs) and their content. Use with caution!"
 
 echo
 echo $USAGE
@@ -12,26 +12,37 @@ STARTING_DIR=`pwd`
 TARGETS=`find $STARTING_DIR -iname 'CVS' -exec echo '{}' ';'`
 
 if [ -z "$TARGETS" ] ; then
-	echo "Nothing to delete, aborting"
+	echo "Nothing to delete, aborting."
 	exit 
 fi
 
 echo
-echo "Spotted directories are :"
+echo "Spotted directories are:"
 find $STARTING_DIR -iname 'CVS' -exec echo '{}' ';'
 
 unset value 
 
 echo
-read -p "Will recursively remove all these CVS-related directories (CVS or cvs) and their content starting from $STARTING_DIR : ok ? (y/n) [n]" value
+read -p "   Will recursively remove all these CVS-related directories (CVS or cvs) and their content starting from $STARTING_DIR: ok? (y/n) [n]" value
 
 if [ "$value" = "y" ] ; then
+
 	echo "Proceeding with deletion...."
 	
-	find $STARTING_DIR -iname 'CVS' -exec rm -rf '{}' ';' 2>/dev/null
+	find $STARTING_DIR -iname 'CVS' -exec /bin/rm -rf '{}' ';' 2>/dev/null
 	
-	echo "... done"
+	if [ $? -eq 0 ] ; then
+	
+		echo "... successfully done"
+	
+	else
+		echo "Error, operation failed." 1>&2
+		exit 5
+	fi
+	
 else
-	echo "Cancelled !"
+
+	echo "Cancelled, nothing done!"
 
 fi	
+
