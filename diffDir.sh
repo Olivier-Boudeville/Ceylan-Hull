91220875965d8d8a8203c6859397c994f2e25a76
Olivier Boudeville <olivier.boudeville@esperide.com>
2008-12-30 21:32:14 +0000

    Enriched and improved.

--- diffDir.sh
+++ diffDir.sh
@@ -4,11 +4,16 @@
 # See diffTree.sh for a recursive comparison.
 
 
-USAGE="\nUsage : `basename $0` <first path (main one)> <second path (to be integrated into first one)> [ -v ] [ -a ] [ -h ] : compares thanks to diff all files which are present both in first and second directories, and warns if they are not identical. Warns too if some files are in one directory but not in the other.\n\t The -v option stands for verbose mode, where identical files are notified too.\n\t The -a option stands for automatic diff editing : a merge tool (default : tkdiff) is triggered whenever a difference is detected, and an editor (default : $EDITOR, otherwise nedit) is triggered to modify the corresponding file being on the first path.\n\t The -h option gives this help."
+USAGE="
+Usage: `basename $0` <first path (main one)> <second path (to be integrated into first one)> [ -v ] [ -a ] [ -h ]: compares thanks to diff all files which are present both in first and second directories, and warns if they are not identical. Warns too if some files are in one directory but not in the other.
+   The -v option stands for verbose mode, where identical files are notified too.
+   The -s option stands for short mode, shorter messages are output.
+   The -a option stands for automatic diff editing: a merge tool (default: tkdiff) is triggered whenever a difference is detected, and an editor (default: $EDITOR, otherwise nedit) is triggered to modify the corresponding file being on the first path.
+   The -h option gives this help."
 
 
 if [ -z "$2" ] ; then
-	echo -e "Error, not enough arguments specified. $USAGE" 1>&2
+	echo "Error, not enough arguments specified. $USAGE" 1>&2
 	exit 1
 fi
 
@@ -31,26 +36,39 @@ fi
 be_verbose=1
 be_quiet=1
 auto_edit=1
+called_as_recursive=1
+shorter_messages=1
 
 
 if [ ! -d "$firstDir" ] ; then
-	echo -e "Error, first directory specified ($firstDir) does not exist. $USAGE"
+	echo "Error, first directory specified ($firstDir) does not exist. $USAGE" 1>&2
 	exit 2
 fi
 
 if [ ! -d "$secondDir" ] ; then
-	echo -e "Error, second directory specified ($secondDir) does not exist. $USAGE"
+	echo "Error, second directory specified ($secondDir) does not exist. $USAGE" 1>&2
 	exit 3
 fi
 
 shift
 shift
 
-while [ "$#" -gt "0" ] ; do
+while [ $# -gt 0 ] ; do
 	token_eaten=1
 	
 	if [ "$1" = "-v" ] ; then
 		be_verbose=0
+		token_eaten=0
+	fi
+	if [ "$1" = "-s" ] ; then
+		shorter_messages=0
+		token_eaten=0
+	fi
+
+	if [ "$1" = "-r" ] ; then
+		# Less repetitive outputs if called recursively:
+		# (see diffTree.sh)
+		called_as_recursive=0
 		token_eaten=0
 	fi
 
@@ -65,19 +83,19 @@ while [ "$#" -gt "0" ] ; do
 	fi
 	
 	if [ "$1" = "-h" ] ; then
-		echo -e "$USAGE"
+		echo "$USAGE"
 		exit
 		token_eaten=0
 	fi
 
-	if [ "$token_eaten" = "1" ] ; then
+	if [ $token_eaten -eq 1 ] ; then
 		echo "Error, unknown argument ($1)." 1>&2
 		exit 4
 	fi	
 	shift
 done
 
-if [ "$auto_edit" = "0" ] ; then
+if [ $auto_edit -eq 0 ] ; then
 	if [ ! -x "$MERGE_TOOL" ] ; then
 		echo "Error, no executable merge tool found ($MERGE_TOOL), automatic diff editing disabled." 1>&2
 		auto_edit=1
@@ -91,49 +109,66 @@ if [ "$auto_edit" = "0" ] ; then
 	
 fi
 
-# To tell a new directory is scanned :
-echo
+# To tell a new directory is scanned:
+echo ${DEFAULT_TEXT}
 
 
-if [ "$be_quiet" = "1" ] ; then
-	echo "Comparing files in $firstDir and $secondDir :"
+if [ $be_quiet -eq 1 ] ; then
+	echo "Comparing files in $firstDir and $secondDir:"
 	echo
 fi
+
 
 for f in `/bin/ls $firstDir`; do
 
 	if [ ! -e "$secondDir/$f" ] ; then
-		echo "${PREFIX_NOEX}$f is only in $firstDir (not in $secondDir).${DEFAULT_TEXT}"
+		if [ $shorter_messages -eq 0 ] ; then
+			echo "${PREFIX_NOEX}$f only in FIRST.${DEFAULT_TEXT}"
+		else
+			echo "${PREFIX_NOEX}$f is only in first directory ($firstDir), i.e. not in $secondDir.${DEFAULT_TEXT}"
+		
+		fi	
 	else
 		if [ ! -d "$firstDir/$f" ] ; then
 		
 			if diff "$firstDir/$f" "$secondDir/$f" 1>/dev/null 2>&1 ; then
-				[ "$be_verbose" = "1" ] || echo "${PREFIX_IDEN}($f identical in the two directories)${DEFAULT_TEXT}"
+				[ $be_verbose -eq 1 ] || echo "${PREFIX_IDEN}($f identical in the two directories)${DEFAULT_TEXT}"
 			else
-				echo "${PREFIX_DIFF} $f differs !${DEFAULT_TEXT}"
-				if [ "$auto_edit" = "0" ]; then 
+				echo "${PREFIX_DIFF} $f differs!${DEFAULT_TEXT}"
+				if [ $auto_edit -eq 0 ]; then 
 					$MERGE_TOOL "$firstDir/$f" "$secondDir/$f" &
 					$EDITOR_TOOL "$firstDir/$f"
 				fi	
 			fi
 		else
 			if [ ! -d "$secondDir/$f" ] ; then
-				echo "${PREFIX_DIFF} $f is a directory in $firstDir and a file in $secondDir !${DEFAULT_TEXT}"
+				echo "${PREFIX_DIFF} $f is a directory in $firstDir and a file in $secondDir!${DEFAULT_TEXT}"
 			fi
 		fi
 	fi	
 done
 
+
 for f in `/bin/ls $secondDir`; do
 
 	if [ ! -e "$firstDir/$f" ] ; then
-		echo "${PREFIX_NOEX}$f is only in $secondDir (not in $firstDir).${DEFAULT_TEXT}"
+		if [ $shorter_messages -eq 0 ] ; then
+			echo "${PREFIX_NOEX}$f only in SECOND.${DEFAULT_TEXT}"
+		else	
+			echo "${PREFIX_NOEX}$f is only in second directory ($secondDir), i.e. not in $firstDir.${DEFAULT_TEXT}"
+		fi	
 	else
 		if [ -d "$secondDir/$f" ] ; then
 			if [ ! -d "$firstDir/$f" ] ; then
-				echo "${PREFIX_DIFF} $f is a file in $firstDir and a directory in $secondDir !${DEFAULT_TEXT}"
+				echo "${PREFIX_DIFF} $f is a file in $firstDir and a directory in $secondDir!${DEFAULT_TEXT}"
 			fi
 		fi
 	fi
 done
+
+
+if [ $called_as_recursive -eq 1 ] ; then
+	[ $be_verbose -eq 0 ] || echo "(use the -v option for more informations)"
+fi
+
 
