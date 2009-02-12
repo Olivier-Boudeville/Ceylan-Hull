9188e826bde4998fb6836930001c230010aab36e
Olivier Boudeville <olivier.boudeville@esperide.com>
2009-02-12 21:20:42 +0000

    Better locale used.

--- putFullDate.sh
+++ putFullDate.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-# Default : in english
-# -f : in french
+# Default: in english
+# -f: in french
 
-# See also : newda script.
+# See also: newda script.
 
 if [ "$1" = "-f" ] ; then
-	LANG=fr_FR
+	LANG=fr
 	date '+%A %-e %B %Y'
 else
 	LANG=
 	date '+%A, %B %-e, %Y'
 fi
+
