9d2e281673ffc462047b786a5195f41bbccfb6f8
Olivier Boudeville <olivier.boudeville@esperide.com>
2009-02-21 21:58:15 +0000

    This file works perfectly.

--- smart-disk-reporter.sh
+++ smart-disk-reporter.sh
@@ -1,16 +1,35 @@
 #!/bin/sh
 
-# To run directly from the command-line:
-# /usr/sbin/smartctl -a -i /dev/sdc 
+# SMART must be enabled.
+# Should be referenced from /etc/SMARTD.conf with an enabled daemon.
+
+
+# More infos:
+# http://lea-linux.org/documentations/index.php/Hardware-hard_plus-smart
+
 
 TEMP_MAIL="$HOME/.smart-disk-reporter.txt"
 
-# Save the mail body from SMART :
+# Saves the mail body sent by smartmontools:
 cat > ${TEMP_MAIL}
 
-/usr/sbin/smartctl -a -d $smartd_DEVICETYPE $smartd_DEVICE >> ${TEMP_MAIL}
+echo "To: $SMARTD_ADDRESS" >> ${TEMP_MAIL}
+echo "Mailer: $SMARTD_MAILER" >> ${TEMP_MAIL}
+echo "Message: $SMARTD_MESSAGE" >> ${TEMP_MAIL}
+echo "Device: $SMARTD_DEVICE" >> ${TEMP_MAIL}
+echo "Device type: $SMARTD_DEVICETYPE" >> ${TEMP_MAIL}
+echo "Device string: $SMARTD_DEVICESTRING" >> ${TEMP_MAIL}
+echo "Fail type: $SMARTD_FAILTYPE" >> ${TEMP_MAIL}
+echo "Date of first error: $SMARTD_TFIRST" >> ${TEMP_MAIL}
+#echo "Number of seconds since 1970: $SMARTD_TFIRSTEPOCH" >> ${TEMP_MAIL}
 
-/usr/bin/mail -s "["`hostname`"] $smartd_SUBJECT" $smartd_ADDRESS < ${TEMP_MAIL}
+echo "
+Complete diagnosis follows:" >> ${TEMP_MAIL}
+/usr/sbin/smartctl -a -d $SMARTD_DEVICETYPE $SMARTD_DEVICE >> ${TEMP_MAIL}
+
+subject="["`hostname`"] $SMARTD_SUBJECT"
+
+cat ${TEMP_MAIL} | /usr/bin/mail -s "$subject" $SMARTD_ADDRESS
+
 /bin/rm -f ${TEMP_MAIL}
-
 
