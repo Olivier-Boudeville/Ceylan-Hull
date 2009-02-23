745c0b64d76f1eb36587745794567bcdabf6bb04
Olivier Boudeville <olivier.boudeville@esperide.com>
2009-02-23 20:13:16 +0000

    Mail sent in case of trouble.

--- debian-updater.sh
+++ debian-updater.sh
@@ -1,21 +1,38 @@
 #!/bin/sh
 
-USAGE="Usage: "`basename $0`": script to be placed in a cron directory (ex : /etc/cron.weekly/), so that the system is regularly and automatically updated. Some regular maintenance by hand is to be performed though, in the case some packages require special settings."
+USAGE="Usage: "`basename $0`": script to be placed in a cron directory (ex: /etc/cron.weekly/), so that the system is regularly and automatically updated. Some regular maintenance by hand is to be performed though, in the case some packages require special settings, and for dist-upgrade. Ensure you have a local mirror in /etc/apt/sources.list, not to load too much main servers"
 
 
-LOG_ROOT=/root/debian-updates
+log_file="$HOME/.debian-update-reporter.txt"
 
-mkdir -p $LOG_ROOT
+apt_opt="-q -y"
 
-LOG_FILE=$LOG_ROOT/`date '+%Y%m%d-%Hh%M'`-debian-update.txt
-
-APT_OPT="-q -y"
+date=`LANG= date`
 
 {
 
-date
-apt-get $APT_OPT update 
-apt-get $APT_OPT upgrade
+apt-get $apt_opt update && apt-get $apt_opt upgrade
+res=$?
 
-} 1>$LOG_FILE 2>&1 
+} 1>$log_file 2>&1 
+
+
+if [ ! $res -eq 0 ] ; then
+
+	subject="["`hostname`"] System update notification"
+
+	host=`hostname`
+
+	mail_file="$HOME/debian-update-last-error.txt"
+	echo "System update triggered an error on $host at $date:" > $mail_file
+
+	
+	cat ${log_file} >> $mail_file
+	cat ${mail_file} | /usr/bin/mail -s "$subject" debian-update@esperide.com
+
+	# Let the mail_file file exist for any further inspection.
+
+fi
+
+/bin/rm -f ${log_file}
 
