b414f6c2f5a6fc139cf0a29c96b8c60896bbe528
Olivier Boudeville <olivier.boudeville@esperide.com>
2009-03-08 14:22:58 +0000

    Better comments added.

--- trimSilence.sh
+++ trimSilence.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-USAGE="  Usage: "`basename $0`" filename
-
+USAGE="
+Usage: "`basename $0`" filename
   Removes any silence at begin and end of specified file, which is updated (initial content is thus lost).
 "
 
@@ -24,7 +24,8 @@ if [ ! -f "${target_file}" ] ; then
 fi
 
 
-sox_tool=`which sox 2>/dev/null`
+# Prefer any recently self-compiled version:
+sox_tool=`PATH=/usr/local/bin:$PATH which sox 2>/dev/null`
 
 if [ ! -x "${sox_tool}" ] ; then
 
@@ -40,6 +41,8 @@ tmp_file=".trimSilence.tmp.wav"
 
 decibel_threshold="-40"
 
+# Reversing to find silences in both directions:
+
 ${sox_tool} ${target_file} ${tmp_file} silence 1 0:0:0.01 ${decibel_threshold}d reverse && ${sox_tool} ${tmp_file} ${target_file} silence 1 0:0:0.01 ${decibel_threshold}d reverse
 
 if [ $? -eq 0 ] ; then
@@ -48,4 +51,7 @@ else
 	echo "Error, trimming of ${target_file} failed." 1>&2
 	exit 20
 fi		
+
+
+/bin/rm -r ${tmp_file}
 
