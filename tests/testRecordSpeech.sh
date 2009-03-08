c7dae499acd4b8290d85d6d29c22e72054180b06
Olivier Boudeville <olivier.boudeville@esperide.com>
2009-03-08 14:23:28 +0000

    Better outputs.

--- tests/testRecordSpeech.sh
+++ tests/testRecordSpeech.sh
@@ -7,15 +7,19 @@ max_voice_count=37
 	
 echo "Testing ${tested_script} with ${max_voice_count} different voices."
 
+/bin/rm -f testRecordSpeech-*.wav
+
 cd ..
 
 voice_count=1
 
 while [ $voice_count -le $max_voice_count ] ; do
 
-	echo "  + testing voice #$voice_count"
+	message="Testing now the voice number $voice_count."
 	
-	${tested_script} --voice-id $voice_count --speech-prefix "tests/testRecordSpeech-$voice_count" --message "Testing now the voice #$voice_count."
+	echo "  + testing voice #$voice_count with '${message}'"
+	
+	${tested_script} --voice-id $voice_count --speech-prefix "tests/testRecordSpeech-$voice_count" --message "${message}"
 	
     voice_count=$(($voice_count+1))
 	
@@ -23,5 +27,5 @@ done
 
 echo "Checking voices are all different indeed: "
 
-md5sum tests/testRecordSpeech*
+md5sum tests/testRecordSpeech-**.wav
 
