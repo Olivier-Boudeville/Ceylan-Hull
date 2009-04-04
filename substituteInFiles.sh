15ce3c8a71cb100db94aca99bd249835d2357a49
Olivier Boudeville <olivier.boudeville@esperide.com>
2009-04-04 18:48:52 +0000

    Script more reliable now.

--- substituteInFiles.sh
+++ substituteInFiles.sh
@@ -1,31 +1,37 @@
 #!/bin/sh
 
 USAGE="
-Usage : substituteInFiles.sh <OldPattern> <NewPattern>
-Substitutes <OldPattern> with <NewPattern> in all files, starting recursively from current directory
-Example : 
+  Usage: substituteInFiles.sh <OldPattern> <NewPattern>
+Substitutes <OldPattern> with <NewPattern> in all files, starting recursively from current directory.
+  Example: 
 	substituteInFiles.sh wondrful wonderful
 	substituteInFiles.sh '<< endl )$' '<< endl ) ;'"
 
+
 if [ ! $# -eq 2 ]; then
-	echo -e $USAGE
-	exit 
+
+	echo $USAGE 1>&2
+	exit 5
+	
 fi
 
-# substitute in all files $1 with $2
-#example : substituteInFiles.sh wondrful wonderful 
 
-echo "Will substitute <$1> with <$2> in following tree : "
+echo "Will substitute <$1> with <$2> in following tree: "
 tree
 
 unset value
-read -p "Let's proceed with substitution ? (y/n) [n]" value
+read -p "Let's proceed with substitution? (y/n) [n]" value
+
 if [ "$value" = "y" ]; then
+
 	echo "Substituing..."
 	find . -type f -name '*' -exec substitute.sh "$1" "$2" '{}' ';'
 	echo "... finished"
+
 else
-	echo "Cancelled"
+
+	echo "Cancelled." 1>&2
+	exit 1
+
 fi
 
-unset value
