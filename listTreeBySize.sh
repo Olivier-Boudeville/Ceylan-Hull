6091bc1e843ecdec86ce4b812f22bd9dbc298dfe
Olivier Boudeville <olivier.boudeville@esperide.com>
2009-04-10 16:31:14 +0000

    Slight reformatting.

--- listTreeBySize.sh
+++ listTreeBySize.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 echo
-echo "Will list all files, starting from current directory, sorted by decreasing size, expressed in kilobytes :"
+echo "Will list all files, starting from current directory, sorted by decreasing size, expressed in kilobytes:"
 echo
 
 find . -type f -exec du -k '{}' ';' | sort -nr | more
+
