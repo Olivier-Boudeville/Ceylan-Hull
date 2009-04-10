7cf7138a3b5a8d800a7b8302f51d0d3182e139e9
Olivier Boudeville <olivier.boudeville@esperide.com>
2009-04-10 21:33:42 +0000

    --announce option added.

--- play-sounds.sh
+++ play-sounds.sh
@@ -1,13 +1,28 @@
 #!/bin/sh
 
-USAGE="  Usage: "`basename $0`" [--quiet] file1 file2 ...
+USAGE="  Usage: "`basename $0`" [ --announce | -a ] [ --quiet | -q] file1 file2 ...
 
   Playbacks specified audio files.
+      --announce: announce the filename that will be played immediatly (with espeak)
       --quiet: not output wanted
 "
 
+espeak=`which espeak`
+
+say()
+{
+
+	#echo "$*"
+	${espeak} "$*"
+	
+}
+
+
 #PLAYER=`which bplay`
 PLAYER=`which mplayer`
+
+# Could be used with mplayer:
+MPLAYER_CMD="${PLAYER} -msglevel identify=6 $f |grep -e '^ID_'|grep -v ID_AUDIO_ID |grep -v ID_DEMUXER |grep -v ID_FILENAME |grep -v ALSA"
 
 if [ ! -x "${PLAYER}" ] ; then
 
@@ -16,9 +31,27 @@ if [ ! -x "${PLAYER}" ] ; then
     
 fi
 
+
+do_announce=1
+
+if [ "$1" = "--announce" -o "$1" = "-a" ] ; then
+	shift
+	
+	if [ ! -x "${espeak}" ] ; then
+	
+		echo "Error, espeak not found." 1>&2
+		exit 15 
+		
+	fi
+	
+	do_announce=0
+fi
+
+
+
 be_quiet=1
 
-if [ "$1" = "--quiet" ] ; then
+if [ "$1" = "--quiet" -o "$1" = "-q" ] ; then
 	shift
 	be_quiet=0
 fi
@@ -33,6 +66,13 @@ for f in $* ; do
     else
     
 		[ $be_quiet -eq 0 ] || echo "  Playing $f"
+		
+		if [ $do_announce -eq 0 ] ; then
+		
+			say "Playing ${f}"
+			
+		fi
+		
         ${PLAYER} ${f} 1>/dev/null 2>&1
         
     fi
