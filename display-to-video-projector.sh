06e9771b4d2a7e965fe75ed02be37c802c583da0
Olivier Boudeville <olivier.boudeville@esperide.com>
2009-06-19 10:59:14 +0000

    Hints for video projectors.

---
+++ display-to-video-projector.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+
+xrandr --output VGA --pos 0x0 --mode 1024x768 --rate 75
+
+# or: xrandr --output VGA --pos 0x0 --mode 1024x768 --same-as LVDS
+# or: xrandr --output VGA --pos 0x0 --rate 75 --mode 1024x768 --same-as LVDS
+
+# or use: System -> Preferences -> Screen Resolution: 
+# it is detected, then once resolution and position are set, restart the 
+# laptop with it plugged in.
+
+# Fn+F4, Fn+F5 and Fn+F7 usually do not work.
+
+# Or: Ctrl+Alt+F2 to switch to a terminal, then Fn+F7 to switch the video
+# output, and Ctrl+Alt+F2 to switch back to the desktop.
+
+# Restore with: xrandr --output LVDS --mode 1280x800
+
