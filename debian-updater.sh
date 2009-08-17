cf6ab947a7f80efa55486bb060e3c55c8f63bd9c
Olivier Boudeville <olivier.boudeville@esperide.com>
2009-08-17 15:04:55 +0000

    Updated with more trace infos.

--- debian-updater.sh
+++ debian-updater.sh
@@ -1,39 +1,58 @@
 #!/bin/sh
 
-USAGE="Usage: "`basename $0`": script to be placed in a cron directory (ex: /etc/cron.weekly/), so that the system is regularly and automatically updated. Some regular maintenance by hand is to be performed though, in the case some packages require special settings, and for dist-upgrade. Ensure you have a local mirror in /etc/apt/sources.list, not to load too much main servers"
+USAGE="Usage: "`basename $0`": script to be placed in a cron directory (ex: /etc/cron.weekly/), so that the system is regularly 
+and automatically updated. Some regular maintenance by hand is to be performed though, in the case some packages require special 
+settings, and for dist-upgrade. Ensure you have a local mirror in /etc/apt/sources.list, not to load too much main servers."
 
 
-log_file="$HOME/.debian-update-reporter.txt"
+log_dir="/root/monitoring"
+
+mkdir -p $log_dir
+
+log_file="$log_dir/debian-update-reporter.txt"
+/bin/rm -f ${log_file}
 
 apt_opt="-q -y"
+
 
 date=`LANG= date`
 
 {
 
-apt-get $apt_opt update && apt-get $apt_opt upgrade
-res=$?
+	apt-get $apt_opt update && apt-get $apt_opt upgrade
+	res=$?
 
-} 1>$log_file 2>&1 
+} 1>$log_file 2>&1
 
 
-if [ ! $res -eq 0 ] ; then
+if [ $res -eq 0 ] ; then
+
+	# Success:
+	
+	touch "$log_dir/debian-update-last-success.timestamp"
+
+	# Log file still there.
+	
+else
+
+	# Failure:
+	
+	touch "$log_dir/debian-update-last-failure.timestamp"
 
 	subject="["`hostname`"] System update notification"
 
 	host=`hostname`
 
 	mail_file="$HOME/debian-update-last-error.txt"
+	
 	echo "System update triggered an error on $host at $date:" > $mail_file
 
-	
 	cat ${log_file} >> $mail_file
 	cat ${mail_file} | /usr/bin/mail -s "$subject" debian-update@esperide.com
 
 	# Let the mail_file file exist for any further inspection.
+	# Log file still there too.
 
 fi
 
-# Let the log_file file to know the date and content of latest update:
-#/bin/rm -f ${log_file}
 
