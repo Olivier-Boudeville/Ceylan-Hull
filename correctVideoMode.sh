c4aa00de534fe36be538238763728a2b1489fb95
Olivier Boudeville <olivier.boudeville@esperide.com>
2009-08-29 18:22:16 +0000

    Resolutions updated.

--- correctVideoMode.sh
+++ correctVideoMode.sh
@@ -6,10 +6,10 @@ XRANDR=/usr/bin/xrandr
 CURRENT_RES=`${XRANDR} | grep '*' | awk '{print $1}'`
 
 # External LCD (instead of 1440x900):
-#TARGET_RES="1680x1050"
+TARGET_RES="1680x1050"
 
 # Laptop LCD:
-TARGET_RES="1280x800"
+#TARGET_RES="1280x800"
 
 
 #echo "Current resolution is ${CURRENT_RES}"
@@ -23,3 +23,4 @@ if [ "${CURRENT_RES}" != "${TARGET_RES}" ] ; then
 else
 	echo "(nothing done, already at target resolution ${TARGET_RES})"
 fi
+
