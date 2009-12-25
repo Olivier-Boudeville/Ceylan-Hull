bc2f6f2e55b8877abe0873a071833fa6b813cd70
Olivier Boudeville <olivier.boudeville@esperide.com>
2009-12-25 22:32:03 +0000

    Script a lot more robust.

--- iptables.rules-TemporaryDisabling.sh
+++ iptables.rules-TemporaryDisabling.sh
@@ -1,21 +1,45 @@
 #/bin/sh
 
+
+firewall_script_name="iptables.rules-Gateway.sh"
+firewall_script=`which ${firewall_script_name}`
+
+if [ ! `id -u` -eq 0 ] ; then
+
+	echo "  
+	Error, you must be root, aborting." 1>&2
+	
+	exit 1
+	
+fi
+
+
+if [ ! -x "${firewall_script}" ] ; then
+
+	echo "  
+	Error, no firewall script found to set back the firewall (${firewall_script_name}), aborting." 1>&2
+	
+	exit 5
+	
+fi
+
 echo "Disabling temporarily ALL iptables rules (beware, all traffic accepted!)"
 
-iptables -X
-iptables -t nat -F
-iptables -t nat -X
-iptables -t mangle -F
-iptables -t mangle -X
-iptables -P INPUT ACCEPT
-iptables -P FORWARD ACCEPT
-iptables -P OUTPUT ACCEPT
+iptables -X && iptables -t nat -F && iptables -t nat -X && iptables -t mangle -F && iptables -t mangle -X && iptables -P INPUT ACCEPT && iptables -P FORWARD ACCEPT && iptables -P OUTPUT ACCEPT
+
+if [ ! $? -eq 0 ] ; then
+
+	echo "  
+	Error, disabling failed, trying to reset directly the rules." 1>&2
+	${firewall_script}
+	
+	exit 15
+
+fi
 
 sleep_duration=1000
 echo "Sleeping for $sleep_duration seconds..."
 sleep $sleep_duration
-
-firewall_script="/etc/init.d/iptables.rules-Gateway.sh"
 
 echo "...awoken, reseting rules with $firewall_script..."
 
