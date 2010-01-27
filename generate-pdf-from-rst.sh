ac7a3698b2b937dac71102828a0b841f73f66781
Olivier Boudeville <olivier.boudeville@esperide.com>
2010-01-27 07:30:22 +0000

    Post clean-up added.

--- generate-pdf-from-rst.sh
+++ generate-pdf-from-rst.sh
@@ -6,7 +6,7 @@ if [ ! -f "${source_file}" ] ; then
 
 	echo "  Error, source file ${source_file} not found." 1>&2
 	exit 10
-	
+
 fi
 
 rule_file="$CEYLAN_SRC/doc/GNUmakerules-docutils.inc"
@@ -15,12 +15,17 @@ if [ ! -f "${rule_file}" ] ; then
 
 	echo "  Error, rule file to generate PDF ${rule_file} not found." 1>&2
 	exit 11
-	
+
 fi
 
-target_file=`echo ${source_file}|sed 's|.rst$|.pdf|1'`
+
+file_prefix=`echo ${source_file}|sed 's|.rst$||1'`
+
+target_file="${file_prefix}.pdf"
 
 echo "
 Generating now ${target_file} from ${source_file}...
 " && make -f "${rule_file}" "${target_file}" && echo "Generation succeeded!"
+
+/bin/rm -f ${file_prefix}.aux ${file_prefix}.tex ${file_prefix}.out ${file_prefix}.log
 
