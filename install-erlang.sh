73e75838c844bbdced6a06ea1aa780689ac60ae4
Olivier Boudeville <olivier.boudeville@esperide.com>
2010-06-04 09:35:44 +0000

    Install as root managed, download avoided if archives are available, better messages.

--- install-erlang.sh
+++ install-erlang.sh
@@ -9,20 +9,22 @@ erlang_md5="ca6da4921e438891967900aa6a084341"
 
 
 usage="Usage: "`basename $0`" [-h|--help] [-c|--cutting-edge] [-d|--doc-install] [-n|--no-download] [<base install directory>]: downloads, builds and installs a fresh Erlang version in specified base directory (if any), or in default directory, and in this case adds a symbolic link pointing to it from its parent directory so that Erlang-current-install always points to the latest installed version.
+Note that, if relevant archives are found in the current directory, they will be used, even if the user did not specified a 'no download' option.
+If this script is run without specifying a base install directory, it should be done preferably from a location like ~/Software/Erlang.
 
 If no base install directory is specified, then if this script is run by root Erlang will be installed into /usr/local (i.e. system-wide), otherwise it will be installed into ~/Software/Erlang/Erlang-${erlang_version}/.
 
 Options:
-    -c or --cutting-edge: use, instead of the latest stable Erlang version, the latest beta version, if any
-    -d or --doc-install: download and install the corresponding documentation as well
-    -n or --no-download: do not attempt to download anything, expect that needed files are already available (useful if not having a direct access to the Internet)
+	-c or --cutting-edge: use, instead of the latest stable Erlang version, the latest beta version, if any
+	-d or --doc-install: download and install the corresponding documentation as well
+	-n or --no-download: do not attempt to download anything, expect that needed files are already available (useful if not having a direct access to the Internet)
 
 Example:
   install-erlang.sh --cutting-edge --doc-install --no-download
-    will install latest available version of Erlang, with its documentation, in the ~/Software/Erlang directory, without downloading anything,
+	will install latest available version of Erlang, with its documentation, in the ~/Software/Erlang directory, without downloading anything,
 	  - or -
   install-erlang.sh --doc-install ~/my-directory
-    will install current official stable version of Erlang, with its documentation, in the ~/my-directory/Erlang/Erlang-${erlang_version} base directory, by downloading Erlang archives from the Internet
+	will install current official stable version of Erlang, with its documentation, in the ~/my-directory/Erlang/Erlang-${erlang_version} base directory, by downloading Erlang archives from the Internet
 
 For Debian-based distributions, you should preferably run beforehand, as root: 'apt-get update && apt-get install gcc make libncurses5-dev openssl libssl-dev', otherwise for example the crypto module might not be available.
 "
@@ -34,6 +36,8 @@ do_download=0
 # By default, will not manage the documentation:
 do_manage_doc=1
 
+# By default, use an installation prefix:
+use_prefix=0
 
 # By default, the Erlang build tree will be removed:
 do_remove_build_tree=0
@@ -46,9 +50,6 @@ erlang_download_location="http://erlang.org/download"
 # Read all known options:
 
 token_eaten=0
-
-# By default, use an installation prefix:
-use_prefix=0
 
 while [ $token_eaten -eq 0 ] ; do
 
@@ -65,6 +66,7 @@ while [ $token_eaten -eq 0 ] ; do
 	fi
 
 
+
 	if [ "$1" = "-c" -o "$1" = "--cutting-edge" ] ; then
 
 		echo "Warning: not using latest beta (unstable) version of Erlang, as the corresponding stable version is more recent."
@@ -75,6 +77,7 @@ while [ $token_eaten -eq 0 ] ; do
 		token_eaten=0
 
 	fi
+
 
 
 	if [ "$1" = "-d" -o "$1" = "--doc-install" ] ; then
@@ -108,22 +111,20 @@ done
 
 if [ -z "$read_parameter" ] ; then
 
-	# Here no base installation directory was specified:
+   # Here no base installation directory was specified:
 
-	if [ `id -u` -eq 0 ] ; then
+   if [ `id -u` -eq 0 ] ; then
 
 		# Run as root, no prefix specified, thus:
-		use_prefix=1
+	   use_prefix=1
+	   prefix="/usr/local"
+	   echo "Run as root, thus using default system installation directory."
 
-		prefix="/usr/local"
-		echo "Run as root, thus using default system installation directory."
+   else
+	   prefix="$HOME/Software/Erlang/Erlang-${erlang_version}"
+	   echo "Not run as root, thus using default installation directory '$prefix'."
 
-	else
-
-		prefix="$HOME/Software/Erlang/Erlang-${erlang_version}"
-		echo "Not run as root, thus using default installation directory '$prefix'."
-
-	fi
+   fi
 
 else
 
@@ -149,51 +150,88 @@ erlang_doc_archive="${erlang_doc_prefix}.tar.gz"
 if [ ! -e "/usr/include/ncurses.h" ] ; then
 
 	echo "  Error, the libncurses headers cannot be found, whereas they are needed for the build.
-		Use for instance 'apt-get install libncurses5-dev' (other packages should preferably be also installed beforehand, see help)." 1>&2
+Use for instance 'apt-get install libncurses5-dev' (other packages should preferably be also installed beforehand, refer to the help message displayed thanks to the -h option)." 1>&2
 
 	exit 5
-
 fi
 
+
+md5sum=`which md5sum`
+
+# Archives are not available by default:
+src_available=1
+doc_available=1
 
 
 if [ $do_download -eq 0 ] ; then
 
-	erlang_target_src_url="${erlang_download_location}/${erlang_src_archive}"
+	# However we check it is not already available in the current directory:
+	if [ -f "$erlang_src_archive" ] ; then
 
-	wget=`which wget`
+		if [ -x "${md5sum}" ] ; then
 
-	if [ ! -x "${wget}" ] ; then
+			md5_res=`${md5sum} ${erlang_src_archive}`
 
-		echo "  Error, no wget tool found, quitting." 1>&2
-		exit 10
+			computed_md5=`echo ${md5_res}|awk '{printf $1}'`
+
+			if [ "${computed_md5}" = "${erlang_md5}" ] ; then
+
+				echo "MD5 sum for the Erlang source archive already locally available validated, not downloading the archive, using that version."
+				src_available=0
+
+			fi
+
+		fi
 
 	fi
 
-	echo "  Downloading now ${erlang_target_src_url}"
-	${wget} ${erlang_target_src_url} 1>/dev/null 2>&1
+	if [ -f "$erlang_doc_archive" ] ; then
 
-	if [ ! $? -eq 0 ] ; then
-		echo "  Error while downloading ${erlang_target_src_url}, quitting." 1>&2
-		exit 15
+		# MD5 checking fo doc not deemed useful:
+		doc_available=0
+
 	fi
 
+	if [ $src_available -eq 1 ] ; then
+
+		erlang_target_src_url="${erlang_download_location}/${erlang_src_archive}"
+
+		wget=`which wget`
+
+		if [ ! -x "${wget}" ] ; then
+
+			echo "  Error, no wget tool found, exiting." 1>&2
+			exit 10
+
+		fi
+
+		echo "Downloading now ${erlang_target_src_url}"
+		${wget} ${erlang_target_src_url} 1>/dev/null 2>&1
+
+		if [ ! $? -eq 0 ] ; then
+			echo "  Error while downloading ${erlang_target_src_url}, quitting." 1>&2
+			exit 15
+		fi
+
+	fi
 
 	erlang_target_doc_url="${erlang_download_location}/${erlang_doc_archive}"
 
 	if [ $do_manage_doc -eq 0 ] ; then
 
+		if [ $doc_available -eq 1 ] ; then
 
-		echo "  Downloading now ${erlang_target_doc_url}"
-		${wget} ${erlang_target_doc_url} 1>/dev/null 2>&1
+			echo "Downloading now ${erlang_target_doc_url}"
+			${wget} ${erlang_target_doc_url} 1>/dev/null 2>&1
 
-		if [ ! $? -eq 0 ] ; then
-			echo "  Error while downloading ${erlang_target_doc_url}, quitting." 1>&2
-			exit 16
+			if [ ! $? -eq 0 ] ; then
+				echo "  Error while downloading ${erlang_target_doc_url}, quitting." 1>&2
+				exit 16
+			fi
+
 		fi
 
 	fi
-
 
 else
 
@@ -218,7 +256,7 @@ else
 fi
 
 
-md5sum=`which md5sum`
+
 
 if [ ! -x "${md5sum}" ] ; then
 
@@ -238,7 +276,6 @@ else
 	fi
 
 fi
-
 
 if [ $use_prefix -eq 0 ] ; then
 
@@ -264,7 +301,6 @@ tar xvzf ${erlang_src_archive}
 if [ ! $? -eq 0 ] ; then
 	echo "  Error while extracting ${erlang_src_archive}, quitting." 1>&2
 	exit 50
-
 fi
 
 initial_path=`pwd`
@@ -289,14 +325,13 @@ if [ $use_prefix -eq 0 ] ; then
 	prefix_opt="--prefix=${prefix}"
 fi
 
-
 echo "  Building Erlang environment..." && ./configure ${configure_opt} ${prefix_opt} && make && make install
 
 
 if [ $? -eq 0 ] ; then
 
 	echo "  Erlang successfully built and installed in ${prefix}.
-		The build tree, in the otp_src_${erlang_version} directory, can be safely removed."
+The build tree, in the otp_src_${erlang_version} directory, can be safely removed."
 
 else
 
@@ -305,15 +340,15 @@ else
 
 fi
 
+
 if [ $use_prefix -eq 0 ] ; then
 
-    # Go to the install (not source) tree:
+	# Go to the install (not source) tree:
 	cd ${prefix}/..
 
-    # Ex: we are in $HOME/Software/Erlang now.
+	# Ex: we are in $HOME/Software/Erlang now.
 
-
-    # Sets as current:
+	# Sets as current:
 	if [ -e Erlang-current-install ] ; then
 
 		/bin/rm -f Erlang-current-install
@@ -323,7 +358,6 @@ if [ $use_prefix -eq 0 ] ; then
 	/bin/ln -sf Erlang-${erlang_version} Erlang-current-install
 
 fi
-
 
 
 if [ $do_manage_doc -eq 0 ] ; then
