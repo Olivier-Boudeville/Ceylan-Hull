041d7ab5ca5e9de5c38709c49d6b124db86fd449
Olivier Boudeville <olivier.boudeville@esperide.com>
2010-08-04 12:04:21 +0000

    Comments improved.

--- listProcessesBySize.sh
+++ listProcessesBySize.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+# Sizes are expressed in Kilobytes.
+
+# Ex:
+# > listProcessesBySize.sh
+#
+#   VSZ CMD
+# 196972 konsole
+# 165548 /usr/lib/firefox-3.0.19/firefox
+# 109668 gaim
+# ...
+
+
 ps -e -o vsize,cmd | grep VSZ | grep -v grep
 ps -e -o vsize,cmd | grep -v VSZ | sort -nr | head -n 30
-
