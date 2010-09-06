30d2b3f99f589d3a9b55f457fbeb3068a141dace
Olivier Boudeville <olivier.boudeville@esperide.com>
2010-09-06 19:09:35 +0000

    Reformatted.

--- correctPathsInTree.sh
+++ correctPathsInTree.sh
@@ -8,9 +8,9 @@ Usage: "`basename $0`" <root of tree whose entry names should be corrected>: ren
 
 if [ $# = "0" ] ; then
 	echo "
-	
+
 	Error, no argument given. $USAGE
-	
+
 	" 1>&2
 	exit 1
 fi
@@ -20,9 +20,9 @@ CORRECTER=`dirname $0`/correctFilename.sh
 
 if [ ! -x "${CORRECTER}" ] ; then
 	echo "
-	
+
 	Error, no executable correcter script found (searched ${CORRECTER}).
-	
+
 	" 1>&2
 	exit 1
 
@@ -33,18 +33,15 @@ TREE_ROOT="$1"
 
 if [ ! -d "${TREE_ROOT}" ] ; then
 	echo "
-	
+
 	Error, no directory named <${TREE_ROOT}> exists. $USAGE
-	
+
 	" 1>&2
 	exit 2
 fi
-	
-	
+
+
 find ${TREE_ROOT} -type d -exec ${CORRECTER} '{}' ';'
 find ${TREE_ROOT} -type f -exec ${CORRECTER} '{}' ';'
 
 echo "Tree fixed."
-
-
-
