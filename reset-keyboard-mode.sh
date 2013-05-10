568eac89135c2ea10d52d187f363aebd142ddcad
Olivier Boudeville <olivier.boudeville@esperide.com>
2013-05-10 10:25:14 +0200

    To fix naughty keyboards!

---
+++ reset-keyboard-mode.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Useful whenever for example VMWare (vmplayer) messes with your keyboard (ex:
+# w.r.t. control key).
+
+# See also: xmodmap and the System-Preferences-Keyboard-Layouts tab.
+
+setxkbmap
+
+# That's it!
