38f07236836a171072ab9678a9600562b2cb5848
Olivier Boudeville <olivier.boudeville@esperide.com>
2013-11-17 13:06:55 +0100

    mangle filter deactivated.

--- iptables.rules-TemporaryDisabling.sh
+++ iptables.rules-TemporaryDisabling.sh
@@ -25,7 +25,17 @@ fi
 
 echo "Disabling temporarily ALL iptables rules (beware, all traffic accepted!)"
 
-iptables -X && iptables -t nat -F && iptables -t nat -X && iptables -t mangle -F && iptables -t mangle -X && iptables -P INPUT ACCEPT && iptables -P FORWARD ACCEPT && iptables -P OUTPUT ACCEPT
+# Mangle removed, to avoid:
+
+#iptables v1.4.20: can't initialize iptables table `mangle': Table does not exist (do you need to insmod?)
+#Perhaps iptables or your kernel needs to be upgraded.
+
+# With mangle:
+#iptables -X && iptables -t nat -F && iptables -t nat -X && iptables -t mangle -F && iptables -t mangle -X && iptables -P INPUT ACCEPT && iptables -P FORWARD ACCEPT && iptables -P OUTPUT ACCEPT
+
+# Without mangle:
+iptables -X && iptables -t nat -F && iptables -t nat -X && iptables -P INPUT ACCEPT && iptables -P FORWARD ACCEPT && iptables -P OUTPUT ACCEPT
+
 
 if [ ! $? -eq 0 ] ; then
 
