276ef521273de9f5155ad8e2dca3963684b065a5
Olivier Boudeville <olivier.boudeville@esperide.com>
2014-02-08 17:49:35 +0100

    Minor reformatting.

--- timer.sh
+++ timer.sh
@@ -21,11 +21,11 @@ else
 		if [ -x "${WAVE_PLAYER_THREE}" ]; then
 			WAVE_PLAYER=${WAVE_PLAYER_THREE}
 		fi
-	fi		
+	fi
 fi
-	
+
 # Can also be obtained from OpenOffice
-# (/usr/lib/openoffice/share/gallery/sounds/gong.wav):	
+# (/usr/lib/openoffice/share/gallery/sounds/gong.wav):
 bong_sound="${LOANI_REPOSITORY}/OSDL-data/gong.wav"
 #time_out_sound="${OSDL_ROOT}/../OSDL-data/dinnerIsReady.wav"
 
@@ -57,16 +57,16 @@ dinnerIsReady()
 
 if [ ! -x "$WAVE_PLAYER" ] ; then
 
-	echo "  Error, no executable wave player found." 1>&2 
+	echo "  Error, no executable wave player found." 1>&2
 	exit 2
-	
+
 fi
 
 if [ ! -f "$time_out_sound" ] ; then
-	
-	echo "  Error, dinner-is-ready sound file not found ($time_out_sound)." 1>&2 
+
+	echo "  Error, dinner-is-ready sound file not found ($time_out_sound)." 1>&2
 	exit 3
-	
+
 fi
 
 
@@ -74,7 +74,7 @@ if [ ! -f "$bong_sound" ] ; then
 
 	echo "  Error, gong sound file not found ($bong_sound)." 1>&2
 	exit 4
-	
+
 fi
 
 
@@ -92,15 +92,15 @@ sleep $waiting_time
 echo "(stopping time: `date '+%H:%M:%S'`)"
 
 echo ".... time is up!"
- 
+
 count=1
 
 while [ "$count" -le "$bong_count" ] ; do
 	bong
 	sleep 1
-    count=$(($count+1))
+	count=$(($count+1))
 done
-   
+
 echo ".... time is up!"
 dinnerIsReady
 
@@ -109,9 +109,7 @@ count=1
 while [ "$count" -le "$bong_count" ] ; do
 	bong
 	sleep 1
-    count=$(($count+1))
+	count=$(($count+1))
 done
 dinnerIsReady
 echo ".... time is up!!!!"
-   
-
