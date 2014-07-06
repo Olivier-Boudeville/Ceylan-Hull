2d6998a38318a0c522faa1e821a35ecd6038271f
Olivier Boudeville <olivier.boudeville@esperide.com>
2014-07-06 12:57:34 +0200

    Improved and clarified.

--- ip-scan.sh
+++ ip-scan.sh
@@ -1,4 +1,23 @@
-PREFIX="192.168"
+#!/bin/sh
+
+USAGE="Usage: $(basename $0) [PREFIX=XXX.YYY] \n
+Scans all IPs with PREFIX, searching for ICMP ping answers. \n
+Ex: $(basename $0) 192.168
+"
+
+if [ $# -ge 2 ] ; then
+
+	echo -e  "$USAGE"
+	exit 5
+
+fi
+
+
+PREFIX="$1"
+
+if [ -z "$PREFIX" ] ; then
+	PREFIX="192.168"
+fi
 
 LOG_FILE="ip-scan.log"
 
@@ -11,21 +30,23 @@ output_message()
 
 }
 
-output_message "Searching responding IP addresses in prefix $PREFIX at "`date`
+output_message "Searching ICMP-responding IP addresses in prefix '$PREFIX' at "`date`
 output_message
 
 A=0
-B=0
+B=1
 
 while [ $A -le 255 ] ; do
+
+	echo " - exploring range $PREFIX.$A.1-255"
 
 	while [ $B -le 255 ] ; do
 		#echo "A=$A, B=$B"
 		TARGET="$PREFIX.$A.$B"
 		#echo "Testing $TARGET"
-		ping $TARGET  -c 1 1>/dev/null
+		ping $TARGET -c 1 1>/dev/null
 		if [ $? -eq 0 ] ; then
-			output_message "++++ Found $TARGET !!!!!"
+			output_message "++++ Found $TARGET!!!!!"
 		else
 			output_message "(nothing at $TARGET)"
 		fi
@@ -33,7 +54,7 @@ while [ $A -le 255 ] ; do
 	done
 
 	A=`expr $A + 1`
-	B=0
+	B=1
 done
 
 output_message "End of search at "`date`
