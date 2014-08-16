4ea0e49bf0e5ce1f61de0c376cb44f5e225d799a
Olivier Boudeville <olivier.boudeville@esperide.com>
2014-08-16 09:00:18 +0200

    No more bad typing due to touchpad.

---
+++ toggle-touchpad-enabling.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+USAGE="Usage: "$(basename $0)": toggles the touchpad activation state."
+
+CLIENT=$(which synclient 2>/dev/null)
+
+if [ ! -x "$CLIENT" ] ; then
+
+	echo "  Error, no synclient found." 1>&2
+
+	exit 5
+
+fi
+
+
+# Quick and dirty, yet working:
+if $CLIENT | grep TouchpadOff | grep '= 1' 1>/dev/null ; then
+
+	$CLIENT TouchpadOff=0
+	echo "Touchpad enabled."
+
+else
+
+	$CLIENT TouchpadOff=1
+	echo "Touchpad disabled."
+
+fi
