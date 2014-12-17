a7d2d163fe392f3ed348a90330422f9c44a2f2f7
Olivier Boudeville <olivier.boudeville@esperide.com>
2014-12-17 11:00:48 +0100

    Minor update.

--- set-time-and-date-by-ntp.sh
+++ set-time-and-date-by-ntp.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-NTP_SERVER=ntp.imag.fr
+NTP_SERVER="ntp.imag.fr"
 
 echo "Setting time and date by NTP thanks to $NTP_SERVER"
 
-if [ `id -u` = "0" ]; then
+if [ $(id -u) = "0" ]; then
+
 	ntpdate -u $NTP_SERVER
+
 else
+
 	echo "You must be root to do that."
+
 fi
