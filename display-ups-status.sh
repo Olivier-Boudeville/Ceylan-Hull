123d0d94f89d11122c4226e2005fe9eef89dccb5
Olivier Boudeville <olivier.boudeville@esperide.com>
2015-04-26 13:28:39 +0200

    Various UPC clients supported.

--- display-ups-status.sh
+++ display-ups-status.sh
@@ -1,9 +1,34 @@
 #!/bin/sh
 
+# An UPS may be queried through NUT (with upsc) or thanks to a vendor-specific
+# client like the APC UPS daemon (with apcaccess).
 
-UPS_NAME="myBelkin"
-UPS_SERVER="aranor"
+UPSC=$(which upsc 2>/dev/null)
+
+if [ -x "${UPSC}" ] ; then
+
+	UPS_NAME="myBelkin"
+	UPS_SERVER="aranor"
+
+	echo "Displaying state of UPS ${UPS_NAME}@${UPS_SERVER}:"
+	$UPSC ${UPS_NAME}@${UPS_SERVER}
+
+else
+
+	APCC=$(which apcaccess 2>/dev/null)
+
+	if [ -x "${APCC}" ] ; then
+
+		echo "Displaying state of APC UPS:"
+		$APCC  status
+
+	else
 
 
-echo "Displaying state of UPS ${UPS_NAME}@${UPS_SERVER}: "
-/bin/upsc ${UPS_NAME}@${UPS_SERVER}
+		echo "  Error, no UPS client found (neither upsc nor apcaccess in the path)." 1>&2
+
+		exit 5
+
+	fi
+
+fi
