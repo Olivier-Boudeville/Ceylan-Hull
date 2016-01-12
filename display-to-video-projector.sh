5db2515c3ba9d8faa29eab6e95a8d691e51b1e7a
Olivier Boudeville <olivier.boudeville@edf.fr>
2016-01-12 17:33:52 +0100

    More examples.

--- display-to-video-projector.sh
+++ display-to-video-projector.sh
@@ -8,8 +8,11 @@
 #xrandr --output VGA-0 --pos 0x0 --mode 1024x768 --rate 60
 xrandr --output VGA-0 --auto --right-of LVDS
 
-# or: xrandr --output VGA --pos 0x0 --mode 1024x768 --same-as LVDS
-# or: xrandr --output VGA --pos 0x0 --rate 75 --mode 1024x768 --same-as LVDS
+# I tend to prefer clones (replacing '--right-of' by '--same-as'):
+
+# xrandr --output VGA --pos 0x0 --mode 1024x768 --same-as LVDS
+# xrandr --output VGA --pos 0x0 --rate 75 --mode 1024x768 --same-as LVDS
+# xrandr --output DP-3 --pos 0x0 --mode 1920x1080 --same-as VGA-0
 
 # or use: System -> Preferences -> Screen Resolution:
 # it is detected, then once resolution and position are set, restart the
