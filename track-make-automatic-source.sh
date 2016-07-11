eb9f87d4654c25316754d497ba706c386fead477
Olivier Boudeville <olivier.boudeville@edf.fr>
2016-07-11 15:32:04 +0200

    Added for RST files.

---
+++ track-make-automatic-source.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+
+USAGE="Usage: $(basename $0) A_TARGET: tracks changes on the source file in order to regenerate the target accordingly."
+
+
+WAITER=$(which inotifywait 2>/dev/null)
+
+if [ ! -x "${WAITER}" ] ; then
+
+	echo "    Error, inotifywait not found; inotify-tools package lacking?" 1>&2
+	exit 5
+
+fi
+
+
+
+if [ ! $# -eq 1 ] ; then
+
+	echo "    Error, exactly one source file must be specified." 1>&2
+	exit 10
+
+fi
+
+
+SOURCE_FILE="$1"
+
+if [ ! -f "${SOURCE_FILE}" ] ; then
+
+	echo "    Error, specified source file '${SOURCE_FILE}' not found." 1>&2
+	exit 15
+
+fi
+
+# Currently supports only *.rst -> *.pdf transformations:
+
+TARGET_FILE=$(echo "${SOURCE_FILE}" | sed 's|.rst$|.pdf|1')
+
+echo "Will track ${SOURCE_FILE}: at each of its modifications the generation of ${TARGET_FILE} will requested..."
+
+while true ; do
+
+	  echo
+	  echo "- regenerating ${TARGET_FILE} on $(date)"
+	  make ${TARGET_FILE}
+	  ${WAITER} -e modify ${SOURCE_FILE} 1>/dev/null 2>&1
+
+done
