169c5e43c0e4f035e59423a91aefce0a08b46d6c
Olivier Boudeville <olivier.boudeville@esperide.com>
2016-07-28 14:22:11 +0200

    Better interface selection and retries.

--- set-usb-tethering.sh
+++ set-usb-tethering.sh
@@ -30,34 +30,82 @@ if [ ! -x "${DHCPCD}" ] ; then
 fi
 
 # Extract for example 'enp0s18f2u1' from '24: enp0s18f2u1: <BROADCAST,MULTICAST...'
-IF_NAME=$($IP addr | grep ': enp' | sed 's|^[[:digit:]]\+\.*\: ||1' | sed 's|\: .*$||1')
+IF_NAME=$($IP addr | grep ': enp0' | sed 's|^[[:digit:]]\+\.*\: ||1' | sed 's|\: .*$||1')
+
+if [ -z "${IF_NAME}" ] ; then
+
+	echo " Error, no relevant network interface found (is USB tethering activated?)." 1>&2
+	#ip addr 1>&2
+	exit 18
+
+fi
 
 #echo "IF_NAME = $IF_NAME"
 
 echo "Using auto-detected interface $IF_NAME..."
 
-# Ensures that the daemon is not already runnning:
-$DHCPCD -k $IF_NAME 1>/dev/null 2>&1
 
-$DHCPCD $IF_NAME 1>/dev/null
+RETRIES=2
 
-if [ $? -eq 0 ] ; then
 
-	if ping -c 1 google.com 1>/dev/null 2>&1 ; then
+connect()
+{
 
-		echo "Connection up and running. Enjoy!"
-		exit 0
+	# Ensures that the daemon is not already runnning:
+	$DHCPCD -k $IF_NAME 1>/dev/null 2>&1
+
+	$DHCPCD $IF_NAME 1>/dev/null
+
+	if [ $? -eq 0 ] ; then
+
+		if test_link ; then
+
+			echo "Connection up and running. Enjoy!"
+			exit 0
+
+		else
+
+			if [ $RETRIES -eq 0 ] ; then
+
+				echo " Error, connection established yet does not seem functional, all retries failed, giving up." 1>&2
+				exit 20
+
+			else
+
+				echo " Connection established yet does not seem functional, retrying..."
+				RETRIES=$(($RETRIES-1))
+				connect
+
+			fi
+
+		fi
 
 	else
 
-		echo " Error, connection established yet does not seem functional." 1>&2
-		exit 30
+		if [ $RETRIES -eq 0 ] ; then
+
+			echo " Error, unable to obtain an IP address from interface, all retries failed, giving up." 1>&2
+			exit 25
+
+		else
+
+			echo "  Unable to obtain an IP address from interface, retrying..."
+			RETRIES=$(($RETRIES-1))
+			connect
+
+		fi
 
 	fi
 
-else
+}
 
-	echo " Error, unable to obtain an IP address from interface." 1>&2
-	exit 20
 
-fi
+test_link()
+{
+
+	sleep 1
+	ping -c 1 google.com 1>/dev/null 2>&1
+	return $?
+}
+
+connect
