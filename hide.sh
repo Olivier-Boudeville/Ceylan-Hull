9d1db5b267db639c03c08284ad98dced6e2e7a7a
Olivier Boudeville <ceylan-commits@esperide.com>
2018-06-04 09:46:26 +0200

    Manages directories now.

--- hide.sh
+++ hide.sh
@@ -1,34 +1,34 @@
 #!/bin/sh
 
 
-USAGE="Usage: $(basename $0) A_FILE: hides specified file (simply by adding a .orig extension to its name)."
+USAGE="Usage: $(basename $0) A_FILE_OR_DIRECTORY: hides specified file or directory (simply by adding a .orig extension to its name)."
 
 if [ ! $# -eq 1 ] ; then
 
-    echo -e "  Error, one argument expected.\n${USAGE}" 1>&2
-    exit 5
+	echo -e "  Error, one argument expected.\n${USAGE}" 1>&2
+	exit 5
 
 fi
 
+# Removes any trailing slash for directories:
+source_element=$(echo "$1" | sed 's|/$||1')
 
-source_file="$1"
+if [ ! -e "${source_element}" ]; then
 
-if [ ! -f "${source_file}" ]; then
-
-    echo -e "  Error, the file '${source_file}' does not exist.\n${USAGE}" 1>&2
-    exit 10
-
-fi
-
-target_file="${source_file}.orig"
-
-if [ -f "${target_file}" ]; then
-
-    echo -e "  Error, the target file '${target_file}' (to be used to hide the original) already exists." 1>&2
-    exit 15
+	echo -e "  Error, the element '${source_element}' does not exist.\n${USAGE}" 1>&2
+	exit 10
 
 fi
 
-/bin/mv "${source_file}" "${target_file}"
+target_element="${source_element}.orig"
 
-echo "('${source_file}' hidden, as '${target_file}')"
+if [ -e "${target_element}" ]; then
+
+	echo -e "  Error, the target element '${target_element}' (to be used to hide the original) already exists." 1>&2
+	exit 15
+
+fi
+
+/bin/mv "${source_element}" "${target_element}"
+
+echo "('${source_element}' hidden, as '${target_element}')"
