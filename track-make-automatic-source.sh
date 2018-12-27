6d531e8500b2c89366eb5cec99397b93a11a44fc
Olivier Boudeville <olivier.boudeville@esperide.com>
2018-12-27 10:32:27 +0100

    Clarified and improved w.r.t. to output selection.

--- track-make-automatic-source.sh
+++ track-make-automatic-source.sh
@@ -1,54 +1,71 @@
 #!/bin/sh
 
+pdf_opt="--pdf"
 
-USAGE="Usage: $(basename $0) A_TARGET: tracks changes on the source file in order to regenerate the target accordingly."
+usage="Usage: $(basename $0) [${pdf_opt}] MY_TARGET_RST_FILE: tracks changes on the RST source file in order to regenerate the target file accordingly. By default, the target file is the corresponding HTML one. Using the ${pdf_opt} option allows to generate the a PDF target instead."
 
 
-WAITER=$(which inotifywait 2>/dev/null)
+waiter=$(which inotifywait 2>/dev/null)
 
-if [ ! -x "${WAITER}" ] ; then
+if [ ! -x "${waiter}" ] ; then
 
 	echo "    Error, inotifywait not found; inotify-tools package lacking?" 1>&2
 	exit 5
 
 fi
 
+target_ext="html"
 
+if [ $# -eq 2 ] ; then
+
+	if [ "$1" = "--pdf" ] ; then
+		echo "Switching to PDF target"
+		target_ext="pdf"
+	else
+		echo "    Error, unexpected option ($1)." 1>&2
+		echo "$usage" 1>&2
+		exit 8
+	fi
+
+	shift
+
+fi
 
 if [ ! $# -eq 1 ] ; then
 
-	echo "    Error, exactly one source file must be specified." 1>&2
+	echo "    Error, exactly one RST source file must be specified." 1>&2
+	echo "$usage" 1>&2
 	exit 10
 
 fi
 
 
-SOURCE_FILE="$1"
+source_file="$1"
 
-if [ ! -f "${SOURCE_FILE}" ] ; then
+if [ ! -f "${source_file}" ] ; then
 
-	echo "    Error, specified source file '${SOURCE_FILE}' not found." 1>&2
+	echo "    Error, specified source file '${source_file}' not found." 1>&2
 	exit 15
 
 fi
 
 # Currently supports only *.rst -> *.pdf transformations:
 
-TARGET_FILE=$(echo "${SOURCE_FILE}" | sed 's|.rst$|.pdf|1')
+target_file=$(echo "${source_file}" | sed "s|.rst$|.${target_ext}|1")
 
-if [ "${SOURCE_FILE}" = "${TARGET_FILE}" ] ; then
+if [ "${source_file}" = "${target_file}" ] ; then
 
-	echo "    Error, source and target files are the same ('${SOURCE_FILE}')." 1>&2
+	echo "    Error, source and target files are the same ('${source_file}')." 1>&2
 	exit 20
 
 fi
 
-echo "Will track ${SOURCE_FILE}: at each of its modifications the generation of ${TARGET_FILE} will requested..."
+echo "Will track ${source_file}: at each of its modifications the generation of ${target_file} will requested..."
 
 # To force a first build (better than a touch detected by the editor):
-if [ -f "${TARGET_FILE}" ] ; then
+if [ -f "${target_file}" ] ; then
 
-	/bin/rm -f "${TARGET_FILE}"
+	/bin/rm -f "${target_file}"
 
 fi
 
@@ -56,8 +73,8 @@ fi
 while true ; do
 
 	  echo
-	  echo "- regenerating ${TARGET_FILE} on $(date)"
-	  make ${TARGET_FILE}
-	  ${WAITER} -e modify ${SOURCE_FILE} 1>/dev/null 2>&1
+	  echo "- regenerating ${target_file} on $(date)"
+	  make ${target_file} && echo "  (${target_file} regenerated)"
+	  ${waiter} -e modify ${source_file} 1>/dev/null 2>&1
 
 done
