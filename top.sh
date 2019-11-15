ec75512d2f7e526d5eac2ab6b986a643fc1a90ac
Olivier Boudeville <ceylan-commits@esperide.com>
2019-11-15 15:00:09 +0100

    htop preferred.

--- top.sh
+++ top.sh
@@ -4,23 +4,44 @@
 # Triggers the best 'top' available: triggers an appropriate tool to monitor
 # processes and system resources.
 
-
-atop=$(which atop 2>/dev/null)
-#echo "atop = ${atop}"
-
+# Currently: htop preferred over atop preferred over classical top.
 
 args="$*"
 #echo "args = ${args}"
 
+# Recreates more or less the usual top:
+default_args=""
 
-if [ -x "${atop}" ] ; then
 
-	#echo " (running ${atop})"
-	${atop} ${args}
+if [ -z "${args}" ]; then
+
+	args="${default_args}"
+
+fi
+
+
+htop=$(which htop 2>/dev/null)
+#echo "htop = ${htop}"
+
+if [ -z "${htop}" ]; then
+
+	atop=$(which atop 2>/dev/null)
+	#echo "atop = ${atop}"
+
+	if [ -z "${atop}" ]; then
+
+		#echo " (running top)"
+		top ${args}
+
+	else
+
+		#echo " (running ${atop})"
+		${atop} ${args}
+
+	fi
 
 else
 
-	#echo " (running top)"
-	top
+	${htop} ${args}
 
 fi
