a9ed68a1bba76874fd987cff9e7ce06ea8cf3687
Olivier Boudeville <olivier.boudeville@esperide.com>
2019-11-24 10:23:50 +0100

    Added for trees.

---
+++ display-tree-stats.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+usage="\nUsage: $(basename $0) ROOT_DIR: displays key stats about specified tree (typically in order to compare merged trees)."
+
+
+root_dir="$1"
+
+if [ -z "${root_dir}" ] ; then
+
+	echo -e "  Error, no root directory specified. ${usage}" 1>&2
+	exit 5
+
+fi
+
+if [ ! -d "${root_dir}" ] ; then
+
+	echo -e "  Error, specified root directory ('${root_dir}') does not exist." 1>&2
+	exit 10
+
+fi
+
+du -sh "${root_dir}"
+tree "${root_dir}" 2>/dev/null | grep -e ' files$'
