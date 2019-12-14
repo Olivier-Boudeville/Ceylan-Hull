ee08296ecf505df34c4f7f0e7763dad11aa16737
Olivier Boudeville <olivier.boudeville@esperide.com>
2019-12-14 15:49:26 +0100

    Updated.

---
+++ adb-pull.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+usage="Usage: $(basename $0) EXPR: allows to download, in the current directory, files based on expressions (typically wildcards) whereas adb pull does not support that.\nEx: $(basename $0) /sdcard/IMG_20191124*jpg"
+
+adb_exec=$(which adb 2>/dev/null)
+
+if [ ! -x "${adb_exec}" ] ; then
+
+	echo "  Error, adb tool not found." 1>&2
+	exit 5
+
+fi
+
+
+args="$*"
+
+if [ -z "${args}" ] ; then
+
+	echo -e "  Error, no argument specified.\n ${usage}" 1>&2
+	exit 15
+
+fi
+
+${adb_exec} shell ls $1 | tr -s "\r\n" "\0" | xargs -0 -n1 ${adb_exec} pull
---
+++ play-audio.sh
@@ -0,0 +1,129 @@
+#!/bin/sh
+
+usage="  Usage: $(basename $0) [--announce|-a] [--quiet|-q] [--recursive|-r] [file1 file2 ...]
+
+  Playbacks audio files as specified:
+	  --announce: announce the filename that will be played immediatly (with espeak)
+	  --quiet: not console output wanted
+	  --recursive: (also) select audio files automatically and recursively, from the current directory
+  (default: no announce, not quiet, not recursive - unless no files are specified)
+"
+
+# See also: just-listen-no-video.sh.
+
+
+espeak=$(which espeak 2>/dev/null)
+
+
+say()
+{
+
+	#echo "$*"
+	${espeak} "$*"
+
+}
+
+
+player=$(which mplayer 2>/dev/null)
+player_opt="-novideo"
+
+#player=$(which cvlc 2>/dev/null)
+#player_opt="--novideo"
+
+
+if [ ! -x "${player}" ] ; then
+
+	echo "Error, no executable player found (${player})." 1>&2
+	exit 5
+
+fi
+
+# Could be used with mplayer:
+mplayer_cmd="${player} -msglevel identify=6 $f |grep -e '^ID_'|grep -v ID_AUDIO_ID |grep -v ID_DEMUXER |grep -v ID_FILENAME |grep -v ALSA"
+
+
+do_announce=1
+be_quiet=1
+be_recursive=1
+
+
+while [ ! $# -eq 0 ] ; do
+
+	if [ "$1" = "--announce" -o "$1" = "-a" ] ; then
+
+		shift
+
+		if [ ! -x "${espeak}" ] ; then
+
+			echo "Error, espeak not found." 1>&2
+			exit 15
+
+		fi
+
+		do_announce=0
+
+	fi
+
+
+	if [ "$1" = "--quiet" -o "$1" = "-q" ] ; then
+		shift
+		be_quiet=0
+	fi
+
+
+	if [ "$1" = "--recursive" -o "$1" = "-r" ] ; then
+		shift
+		be_recursive=0
+	fi
+
+done
+
+
+#echo "do_announce=${do_announce}"
+#echo "be_quiet=${be_quiet}"
+#echo "be_recursive=${be_recursive}"
+
+files="$*"
+
+if [ ${be_recursive} -eq 0 ] || [ -z "${files}" ]; then
+
+	echo " (playing recursively from $(pwd))"
+
+	files="${files} $(find . -iname '*.wav' -o -iname '*.ogg' -o -iname '*.mp3' 2>/dev/null)"
+
+fi
+
+#echo "files=${files}"
+
+
+for f in ${files} ; do
+
+	if [ ! -f "${f}" ] ; then
+
+		echo "  (file ${f} not found, thus skipped)" 1>&2
+
+	else
+
+		[ $be_quiet -eq 0 ] || echo "  - playing now ${f}"
+
+		if [ $do_announce -eq 0 ] ; then
+
+			say "Playing ${f}"
+
+		fi
+
+		${player} ${player_opt} ${f} 1>/dev/null 2>&1
+
+		# Useful to stop the overall reading as a whole:
+		if [ ! $? -eq 0 ] ; then
+
+			echo "Playback of ${f} failed, stopping." 1>&2
+			exit 20
+
+		fi
+
+	fi
+
+done
+
+echo " (end of playback)"
