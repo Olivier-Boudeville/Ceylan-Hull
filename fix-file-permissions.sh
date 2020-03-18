aeeb47205e579429e3600b0a2d932583008a4226
Olivier Boudeville <olivier.boudeville@esperide.com>
2020-03-18 15:54:36 +0100

    GPG added.

--- fix-file-permissions.sh
+++ fix-file-permissions.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-USAGE="Usage: $(basename $0): corrects the UNIX permissions for the most common file extensions for all files in the current directory."
+USAGE="Usage: $(basename $0): corrects the UNIX permissions for the most common file extensions, for all files in the current directory."
 
 
 echo "Correcting file permissions in $(pwd)..."
 
-for f in *.doc *.docx *.xls *.xlsx *.ppt *.pptx *.pdf *.txt *.zip *.png *.jpg *.jpeg *.gz *.tgz *.bz2 *.xz *.rst *.mp4 ; do
+for f in *.doc *.docx *.xls *.xlsx *.ppt *.pptx *.pdf *.txt *.zip *.png *.jpg *.jpeg *.gz *.tgz *.bz2 *.xz *.rst *.mp4 *.gpg ; do
 
 	if [ -f "$f" ] ; then
 
