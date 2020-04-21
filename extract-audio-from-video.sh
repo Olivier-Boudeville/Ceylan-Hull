a95e7c651db87dd51ec87a644bace2b7a7c6d591
Olivier Boudeville <olivier.boudeville@esperide.com>
2020-04-21 21:36:16 +0200

    Added.

---
+++ extract-audio-from-video.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+
+# Same content being playback, knowing the actual CPU time used by a process is
+# user+sys:
+#
+# mp4: real 188,214	user 124,142	sys 63,560	pcpu 99,73
+# ogg: real 188,258	user 1,340		sys 0,686	pcpu 1,07
+#
+# 124+63 = 187 vs 2, so ~93 times more demanding as mp4, and thus such
+# extraction is worthwhile.
+
+encoder_name="ffmpeg"
+
+encoder=$(which ${encoder_name} 2>/dev/null)
+
+if [ ! -x "${encoder}" ]; then
+
+	echo "  Error, encoder '${encoder_name}' not found." 1>&2
+
+	exit 5
+
+fi
+
+
+video_file="$1"
+
+if [ ! -f "${video_file}" ]; then
+
+	echo "  Error, input file '${video_file}' not found." 1>&2
+
+	exit 10
+
+fi
+
+audio_file=$(echo "${video_file}" | sed 's|\.mp4|.ogg|1')
+
+echo "video file:${video_file}, audio one: ${audio_file}"
+
+# -vn: disable video recording
+# -acodec: set thes audio codec
+# -y: overwrite output files without asking.
+# -aq: set the audio quality; http://wiki.hydrogenaud.io/index.php?title=Recommended_Ogg_Vorbis#Recommended_Encoder_Settings
+#
+${encoder} -i "${video_file}" -vn -acodec libvorbis -aq 5 -y "${audio_file}"
