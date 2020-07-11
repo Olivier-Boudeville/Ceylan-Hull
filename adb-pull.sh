d67dc606316c036c0a0b5e9433a65f3998c54bf9
Olivier Boudeville <olivier.boudeville@esperide.com>
2020-07-11 18:25:27 +0200

    Comments added.

--- adb-pull.sh
+++ adb-pull.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-usage="Usage: $(basename $0) EXPR: allows to download in the current directory, from the already connected and authorizing Android device (typically mobile phone), files based on the specified expression(s) (typically wildcards) whereas adb pull does not support that.\nEx: $(basename $0) /sdcard/DCIM/Camera/IMG_20200530*.jpg"
+usage="Usage: $(basename $0) EXPR: allows to download in the current directory, from the already connected and authorizing Android device (typically mobile phone), files based on the specified expression(s) (typically wildcards), knowing that adb pull does not support that.\nEx: $(basename $0) /sdcard/DCIM/Camera/IMG_20200530*.jpg"
 
 # To find content (ex: snapshots) in mobile phone:
 # $ adb shell
 # $ find "/sdcard/" -iname "*.jpg"
 #
 # Typically found as: /sdcard/DCIM/Camera/IMG_20200525_175458.jpg
-
+#
+# So a typical command-line may be:
+#   adb-pull.sh /sdcard/DCIM/Camera/IMG_20200710*
+#
+# See also our fix-snapshots.sh script.
 
 adb_exec=$(which adb 2>/dev/null)
 
