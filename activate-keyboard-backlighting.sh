eaed6a931628ace16df515ea24ae8761e017210a
Olivier Boudeville <olivier.boudeville@esperide.com>
2020-08-06 11:32:22 +0200

    Better report of current brightness.

--- activate-keyboard-backlighting.sh
+++ activate-keyboard-backlighting.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-usage="Usage: $(basename $0) [level]: activates the keyboard backlighting (0% for level 1, 50% for level 1, 100% for level 2 (the default)."
+usage="Usage: $(basename $0) [-h|--help] [level]: activates the keyboard backlighting; for level 0: 0%, i.e. switched off; for level 1: 50%; for level 2 (the default, if none is specified): 100%."
 
 #echo $usage
 
-if [ ! $(id -u) -eq 0 ] ; then
+if [ ! $(id -u) -eq 0 ]; then
 
 	echo "  Error, you must be root." 1>&2
 	exit 5
 
 fi
 
-CTRL=$(which brightnessctl 2>/dev/null)
+ctrl_tool=$(which brightnessctl 2>/dev/null)
 
-if [ ! -x "${CTRL}" ] ; then
+if [ ! -x "${ctrl_tool}" ]; then
 
 	echo "  Error, 'brightnessctl' tool not available. One may use: 'pacman -Sy brightnessctl'." 1>&2
 	exit 10
@@ -21,9 +21,13 @@ if [ ! -x "${CTRL}" ] ; then
 fi
 
 
+device_opt=--device='tpacpi::kbd_backlight'
+
+echo "(current brightness level is $(${ctrl_tool} ${device_opt} get))"
+
 level="$1"
 
-if [ "${level}" = "-h" ] ; then
+if [ "${level}" = "-h" ] ||  [ "${level}" = "--help" ]; then
 
 	echo ${usage}
 
@@ -32,18 +36,18 @@ if [ "${level}" = "-h" ] ; then
 fi
 
 
-if [ -z "${level}" ] ; then
+if [ -z "${level}" ]; then
 
 	level=2
 
 fi
 
 echo "Setting brightness level to ${level}."
-${CTRL} --device='tpacpi::kbd_backlight' set ${level}
+${ctrl_tool} ${device_opt} set ${level}
 
 res=$?
 
-if [ $res -eq 0 ] ; then
+if [ $res -eq 0 ]; then
 
 	echo "(success)"
 
