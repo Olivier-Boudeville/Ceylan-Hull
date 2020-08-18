878fb8a2017974d318015323ade6140862162332
Olivier Boudeville <olivier.boudeville@esperide.com>
2020-08-18 16:17:32 +0200

    Compliant with ignored packages.

---
+++ stop-local-host.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+if [ ! $(id -u) -eq 0 ]; then
+
+	echo "  Error, this script must be run as root." 1>&2
+	exit 5
+
+fi
+
+
+echo "
+
+System will shutdown now host $(hostname -s), after a possible update (including notably its kernel)...
+"
+
+read -e -p "  Press Enter key to continue (CTRL-C to abort)" value
+
+
+# Basic update:
+pacman --noconfirm -Sy
+
+if [ ! $? -eq 0 ]; then
+
+	echo "  Error, general update failed." 1>&2
+	exit 5
+
+fi
+
+# As we shutdown, updating the kernel is not a problem regarding modules that
+# are yet loaded:
+#
+# (overrides any IgnorePkg directive in /etc/pacman.conf)
+#
+pacman --noconfirm --needed -Sy linux linux-headers
+
+if [ ! $? -eq 0 ]; then
+
+	echo "  Error, kernel update failed." 1>&2
+	exit 10
+
+fi
+
+echo "Stopping now."
+shutdown -h now
+
+# Not expected to be seen:
+echo "Stopped."
