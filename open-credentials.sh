c4410f2e2be19e5ef4815b023b69f14dfa4d068c
Olivier Boudeville <olivier.boudeville@esperide.com>
2020-08-25 09:52:16 +0200

    Minor style update.

--- open-credentials.sh
+++ open-credentials.sh
@@ -2,7 +2,8 @@
 
 # (not sh, as we prefer 'read' to support a "no echo" option)
 
-USAGE="  Usage: $(basename $0)
+
+usage="  Usage: $(basename $0)
   Unlocks (decrypts) the credential file whose path is read from the user environment, and opens it. Once closed, re-locks it (with the same passphrase). See also: {lock|unlock}-credentials.sh."
 
 
@@ -10,10 +11,10 @@ crypt_tool_name="gpg"
 
 crypt_tool=$(which $crypt_tool_name 2>/dev/null)
 
-if [ ! -x "$crypt_tool" ] ; then
+if [ ! -x "$crypt_tool" ]; then
 
 	echo "  Error, no encryption tool found (no $crypt_tool_name)." 1>&2
-	exit 10
+	exit 5
 
 fi
 
@@ -23,10 +24,10 @@ shred_tool_name="shred"
 
 shred_tool=$(which $shred_tool_name 2>/dev/null)
 
-if [ ! -x "$shred_tool" ] ; then
+if [ ! -x "$shred_tool" ]; then
 
 	echo "  Error, no shredding tool found (no $shred_tool_name)." 1>&2
-	exit 11
+	exit 10
 
 fi
 
@@ -36,7 +37,7 @@ env_file="$HOME/.ceylan-settings.txt"
 if [ ! -f "${env_file}" ]; then
 
 	echo "  Error, no environment file (${env_file}) found." 1>&2
-	exit 5
+	exit 15
 
 fi
 
@@ -44,30 +45,30 @@ fi
 # Used to rely on a shell-compliant syntax, now Erlang one:
 #source "${env_file}"
 
-MAIN_CREDENTIALS_PATH=$(/bin/cat ${env_file} | grep -v % | grep main_credentials_path | sed 's|.*, "||1' | sed 's|" }.$||1')
+main_credentials_path=$(/bin/cat ${env_file} | grep -v % | grep main_credentials_path | sed 's|.*, "||1' | sed 's|" }.$||1')
 
 
-if [ -z "${MAIN_CREDENTIALS_PATH}" ] ; then
+if [ -z "${main_credentials_path}" ]; then
 
 	echo "  Error, no usable main_credentials_path key entry found in environment file (${env_file})." 1>&2
-	exit 6
+	exit 20
 
 fi
 
 
-unlocked_file="${MAIN_CREDENTIALS_PATH}.dat"
-locked_file="${MAIN_CREDENTIALS_PATH}"
+unlocked_file="${main_credentials_path}.dat"
+locked_file="${main_credentials_path}"
 
 
 # Expected to be found locked:
 already_unlocked=1
 
 
-if [ ! -f "${locked_file}" ] ; then
+if [ ! -f "${locked_file}" ]; then
 
-	if [ -f "${unlocked_file}" ] ; then
+	if [ -f "${unlocked_file}" ]; then
 
-		echo "  Warning: the credentials file (as defined in the MAIN_CREDENTIALS_PATH variable of the environment file '${env_file}') was already unlocked (its unlocked version, '${unlocked_file}', already exists, whereas its locked version, '${locked_file}', does not exist). As a result, a password (to be chosen identical to the usual one) will be requested (twice) when closing this file." 1>&2
+		echo "  Warning: the credentials file (as defined in the main_credentials_path variable of the environment file '${env_file}') was already unlocked (its unlocked version, '${unlocked_file}', already exists, whereas its locked version, '${locked_file}', does not exist). As a result, a password (to be chosen identical to the usual one) will be requested (twice) when closing this file." 1>&2
 		# This happens whenever left in a terminal, being forgotten, or closing
 		# the terminal while still opened:
 		#
@@ -75,9 +76,9 @@ if [ ! -f "${locked_file}" ] ; then
 
 	else
 
-		echo "  Error, no credentials file (as defined in the MAIN_CREDENTIALS_PATH variable of the environment file '${env_file}') can be found (neither in a locked version, i.e. as '${locked_file}', nor in an unlocked version, i.e. as '${unlocked_file}')." 1>&2
+		echo "  Error, no credentials file (as defined in the main_credentials_path variable of the environment file '${env_file}') can be found (neither in a locked version, i.e. as '${locked_file}', nor in an unlocked version, i.e. as '${unlocked_file}')." 1>&2
 
-		exit 21
+		exit 25
 
 	fi
 
@@ -85,11 +86,11 @@ else
 
 	# So here the locked file exists.
 
-	if [ -f "${unlocked_file}" ] ; then
+	if [ -f "${unlocked_file}" ]; then
 
-		echo "  Error, the credentials file (as defined in the MAIN_CREDENTIALS_PATH variable of the environment file '${env_file}') exists both in its locked version ('${locked_file}') and in its unlocked version ('${unlocked_file}'), this is abnormal." 1>&2
+		echo "  Error, the credentials file (as defined in the main_credentials_path variable of the environment file '${env_file}') exists both in its locked version ('${locked_file}') and in its unlocked version ('${unlocked_file}'), this is abnormal." 1>&2
 
-		exit 22
+		exit 30
 
 	fi
 
@@ -97,7 +98,7 @@ fi
 
 # And here the unlocked file does not exist yet.
 
-if [ $already_unlocked -eq 1 ] ; then
+if [ $already_unlocked -eq 1 ]; then
 
 	#echo "Unlocking credentials: from ${locked_file} to ${unlocked_file}."
 
@@ -118,7 +119,7 @@ if [ $already_unlocked -eq 1 ] ; then
 
 	res="$?"
 
-	if [ $res -eq 0 ] ; then
+	if [ $res -eq 0 ]; then
 
 		#echo "(credentials unlocked in ${unlocked_file})"
 		echo "(credentials unlocked)"
@@ -126,20 +127,20 @@ if [ $already_unlocked -eq 1 ] ; then
 		${shred_tool} --force --remove --zero "${locked_file}"
 		res="$?"
 
-		if [ ! $res -eq 0 ] ; then
+		if [ ! $res -eq 0 ]; then
 
-			echo "Error, shredding of '${locked_file}' failed (code: $res), removing it." 1>&2
+			echo "  Error, shredding of '${locked_file}' failed (code: $res), removing it." 1>&2
 			/bin/rm -f "${locked_file}"
 
-			exit 10
+			exit 35
 
 		fi
 
 	else
 
-		echo "Error, unlocking failed (code: $res), stopping, locked file '${locked_file}' left as it is." 1>&2
+		echo "  Error, unlocking failed (code: $res), stopping, locked file '${locked_file}' left as it is." 1>&2
 
-		exit 11
+		exit 40
 
 	fi
 
@@ -164,11 +165,11 @@ res="$?"
 
 unset passphrase
 
-if [ ! $res -eq 0 ] ; then
+if [ ! $res -eq 0 ]; then
 
-	echo "Error, locking failed (code: $res), stopping, unlocked file '${unlocked_file}' left as it is, please lock it manually." 1>&2
+	echo "  Error, locking failed (code: $res), stopping, unlocked file '${unlocked_file}' left as it is, please lock it manually." 1>&2
 
-	exit 11
+	exit 45
 
 fi
 
@@ -179,19 +180,19 @@ fi
 #chmod 000 ${locked_file}
 chmod 400 ${locked_file}
 
-if [ $res -eq 0 ] ; then
+if [ $res -eq 0 ]; then
 
 	#echo "(credentials locked in ${locked_file})"
 	echo "(credentials locked)"
 	${shred_tool} --force --remove --zero "${unlocked_file}"
 	res="$?"
 
-	if [ ! $res -eq 0 ] ; then
+	if [ ! $res -eq 0 ]; then
 
-		echo "Error, shredding of '${unlocked_file}' failed (code: $res), removing it." 1>&2
+		echo "  Error, shredding of '${unlocked_file}' failed (code: $res), removing it." 1>&2
 		/bin/rm -f "${unlocked_file}"
 
-		exit 10
+		exit 50
 
 	fi
 
@@ -199,6 +200,6 @@ else
 
 	echo "  Error, locking failed (code: $res), stopping, unlocked file '${unlocked_file}' left as it is." 1>&2
 
-	exit 11
+	exit 55
 
 fi
