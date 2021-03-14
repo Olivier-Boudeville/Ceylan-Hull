1e8aeab617087071c762f1b31a1e2a48a0420e21
Olivier Boudeville <olivier.boudeville@esperide.com>
2021-03-14 16:23:30 +0100

    Help added.

--- list-tags-by-date.sh
+++ list-tags-by-date.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-usage="Usage: $(basename $0): lists, for the current VCS (GIT) repository, all (annotated) tags, from the oldest one to the latest one."
+usage="Usage: $(basename $0) [-h|--help]: lists, for the current VCS (GIT) repository, all (annotated) tags, from the oldest one to the latest one."
+
+
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+
+	echo "${usage}"
+	exit 0
+
+fi
 
 echo "  Listing repository tags, from oldest to newest:"
 
