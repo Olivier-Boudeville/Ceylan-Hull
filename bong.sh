db6855398e6f83c1441df0088a8d407f77b31fd5
Olivier Boudeville <olivier.boudeville@esperide.com>
2021-04-20 10:41:29 +0200

    Use of VLC now.

--- bong.sh
+++ bong.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-usage="Usage: $(basename $0) [COUNT]: plays COUNT (default: 1) bong sound(s)."
+usage="Usage: $(basename $0) [COUNT]: plays COUNT (default: 1) bong sound(s). Useful for example to notify the end of a longer shell operation."
 
 
 bong()
 {
-	${audio_player} ${bong_sound} 1>/dev/null 2>&1
+	echo ${audio_player_cmd} "${bong_sound}" #1>/dev/null 2>&1
 	echo "Bong!"
 }
 
 
-audio_player=$(which playwave 2>/dev/null)
+audio_player=$(which cvlc 2>/dev/null)
+audio_player_cmd="${audio_player} --quiet --novideo --play-and-exit"
 
 bong_sound="${LOANI_REPOSITORY}/OSDL-data/gong.wav"
 
@@ -21,25 +22,26 @@ if [ -z "${bong_count}" ]; then
 fi
 
 
-if [ ! -x "${audio_player}" ] ; then
+if [ ! -x "${audio_player}" ]; then
 	audio_player=$(which mplayer 2>/dev/null)
+	audio_player_cmd="${audio_player}"
 fi
 
 
-if [ ! -x "$audio_player" ] ; then
+if [ ! -x "${audio_player}" ]; then
 	echo "No executable audio player found."
 	exit 2
 fi
 
-if [ ! -f "$bong_sound" ] ; then
-	echo "Gong sound file not found ($bong_sound)."
+if [ ! -f "${bong_sound}" ]; then
+	echo "Gong sound file not found (${bong_sound})."
 	exit 3
 fi
 
 
 count=1
 
-while [ "$count" -le "$bong_count" ] ; do
+while [ ${count} -le ${bong_count} ]; do
 
 	bong
 	sleep 1
