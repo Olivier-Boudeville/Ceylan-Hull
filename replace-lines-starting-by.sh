18484fdf17723d1925ebd2de9345a2f38528598a
Olivier Boudeville <olivier.boudeville@esperide.com>
2021-04-30 21:51:15 +0200

    Robustified.

--- replace-lines-starting-by.sh
+++ replace-lines-starting-by.sh
@@ -1,8 +1,7 @@
 #/bin/sh
 
 usage="
-Usage: $(basename $0) START TARGET FILE
-		.
+Usage: $(basename $0) START_OF_LINE TARGET_LINE TARGET_FILE
 
 Example: $(basename $0) 'MAKE=' 'MAKE=/usr/bin/make' myFile"
 
@@ -10,31 +9,32 @@ Example: $(basename $0) 'MAKE=' 'MAKE=/usr/bin/make' myFile"
 
 
 if [ $# != 3 ]; then
-	echo "${usage}" 1>&2
-	exit 1
+	echo " Error, 3 parameters expected.
+${usage}" 1>&2
+	exit 5
 fi
 
 
 target_file="$3"
 
 if [ ! -f "${target_file}" ]; then
-	echo "  Cannot operate on ${target_file}, which is not a regular file." 1>&2
-	exit 2
+	echo "  Error, cannot operate on '${target_file}', which is not an existing regular file." 1>&2
+	exit 10
 fi
 
-shell_dir=$(dirname $0)
+shell_dir="$(dirname $0)"
 
-SOURCE="^$(${shell_dir}/protect-special-characters.sh "$1").*$"
-TARGET=$(${shell_dir}/protect-special-characters.sh "$2")
+source="^$(${shell_dir}/protect-special-characters.sh "$1").*$"
+target_line=$(${shell_dir}/protect-special-characters.sh "$2")
 
-#echo "SOURCE = $SOURCE"
-#echo "TARGET = $TARGET"
-#echo "FILE   = $3"
+#echo "source = ${source}"
+#echo "target_line = ${target_line}"
+#echo "target_file = ${target_file}"
 
 temp_file=".replace-lines-starting-by.tmp"
 
-/bin/cp -f ${target_file} ${temp_file}
+/bin/cp -f "${target_file}" "${temp_file}"
 
-/bin/cat ${temp_file} | sed -e "s|$SOURCE|$TARGET|g" > ${target_file}
+/bin/cat "${temp_file}" | sed -e "s|${source}|${target_line}|g" > "${target_file}"
 
-/bin/rm -f ${temp_file}
+/bin/rm -f "${temp_file}"
