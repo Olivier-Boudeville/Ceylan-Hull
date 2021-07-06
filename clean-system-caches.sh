e16f84628fae70164148e18735784e6cd7d562fd
Olivier Boudeville <olivier.boudeville@esperide.com>
2021-07-06 22:08:27 +0200

    Automatisation fixed.

--- clean-system-caches.sh
+++ clean-system-caches.sh
@@ -1,9 +1,44 @@
 #!/bin/sh
 
-if [ $(id -u) -eq 0 ]; then
-	echo " Cleaning system caches: full removal of the content of pacman cache..."
-	pacman -Scc --noconfirm 1>/dev/null && echo "  ... success!"
-else
-	echo "Error, you must be root." 1>&2
+usage="Usage: $(basename $0) [-h|--help]: cleans all system caches, typically to try to save some space from /var/cache."
+
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+
+	echo "${usage}"
+
+	exit 0
+
+fi
+
+if [ -n "$1" ]; then
+
+	echo "  Error, no parameter expected.
+${usage}" 1>&2
+
 	exit 5
+
+fi
+
+
+if [ $(id -u) -eq 0 ]; then
+
+	echo " Cleaning system caches: full removal of the content of pacman cache..."
+
+	# Note that the --noconfirm option would just apply the default answer,
+	# which is "no" in some cases:
+	#
+	# """
+	# :: Do you want to remove ALL files from cache? [y/N] y
+	# :: Do you want to remove unused repositories? [Y/n] y
+	#
+	# So:
+	#
+	( yes | pacman -Scc 1>/dev/null ) && echo "  ... success!"
+
+else
+
+	echo "  Error, you must be root." 1>&2
+
+	exit 15
+
 fi
