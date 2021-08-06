1a43e61b02d83fc415203b5f6b76cf30b3d8407a
Olivier Boudeville <olivier.boudeville@esperide.com>
2021-08-06 10:40:15 +0200

    Robustified and updated.

--- set-time-and-date-by-ntp.sh
+++ set-time-and-date-by-ntp.sh
@@ -5,19 +5,38 @@ usage="Usage: $(basename $0) [NTP_SERVER]: sets time and date by NTP thanks to s
 ntp_server="$1"
 
 if [ -z "${ntp_server}" ]; then
-	ntp_server="ntp.imag.fr"
-fi
 
+	# In most cases it is best to use:
+	ntp_server="pool.ntp.org"
+
+	#ntp_server="server 0.fr.pool.ntp.org"
+	#ntp_server="server 1.fr.pool.ntp.org"
+	#ntp_server="server 2.fr.pool.ntp.org"
+	#ntp_server="server 3.fr.pool.ntp.org"
+
+	#ntp_server="ntp.imag.fr"
+
+fi
 
 if [ $(id -u) = "0" ]; then
 
 	echo "Setting time and date by NTP thanks to ${ntp_server}..."
-	ntpdate -u ${ntp_server}
+	if ntpdate -u ${ntp_server}; then
+
+		echo "Success!"
+
+	else
+
+		echo "Synchronisation failed." 1>&2
+
+		exit 5
+
+	fi
 
 else
 
 	echo "Error, you must be root to do that." 1>&2
 
-	exit 5
+	exit 15
 
 fi
