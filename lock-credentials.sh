6cba12090ecd0e075a6bfcc022a2974a986b8c14
Olivier Boudeville <olivier.boudeville@esperide.com>
2021-08-25 14:20:35 +0200

    Credentials robustified.

--- lock-credentials.sh
+++ lock-credentials.sh
@@ -3,17 +3,28 @@
 usage="Usage: $(basename $0): locks (encrypts) the credential file whose path is read from the user environment. See also: unlock-credentials.sh."
 
 # See also the open-credentials.sh integrated script.
+#
+# The present script is mostly useful whenever the previous one fails for any
+# reason (ex: a text editor messing with the open/close detection of that
+# credential file).
 
-# Mostly useful whenever it fails for any reason.
 
+if [ ! $# -eq 0 ]; then
+
+	echo "  Error, no parameter shall be specified.
+${usage}" 1>&2
+
+	exit 2
+
+fi
 
 crypt_tool_name="gpg"
 
-crypt_tool=$(which $crypt_tool_name 2>/dev/null)
+crypt_tool="$(which ${crypt_tool_name} 2>/dev/null)"
 
-if [ ! -x "$crypt_tool" ]; then
+if [ ! -x "${crypt_tool}" ]; then
 
-	echo "  Error, no encryption tool found (no $crypt_tool_name)." 1>&2
+	echo "  Error, no encryption tool found (no '${crypt_tool_name}')." 1>&2
 	exit 10
 
 fi
@@ -22,11 +33,11 @@ fi
 
 shred_tool_name="shred"
 
-shred_tool=$(which $shred_tool_name 2>/dev/null)
+shred_tool="$(which ${shred_tool_name} 2>/dev/null)"
 
-if [ ! -x "$shred_tool" ]; then
+if [ ! -x "${shred_tool}" ]; then
 
-	echo "  Error, no shredding tool found (no $shred_tool_name)." 1>&2
+	echo "  Error, no shredding tool found (no '${shred_tool_name}')." 1>&2
 	exit 11
 
 fi
@@ -36,34 +47,34 @@ env_file="$HOME/.ceylan-settings.txt"
 
 if [ ! -f "${env_file}" ]; then
 
-	echo "  Error, no environment file (${env_file}) found." 1>&2
+	echo "  Error, no environment file ('${env_file}') found." 1>&2
 	exit 5
 
 fi
 
-MAIN_CREDENTIALS_PATH=$(/bin/cat ${env_file} | grep main_credentials_path | sed 's|.*, "||1' | sed 's|" }.$||1')
+main_credentials_path="$(/bin/cat ${env_file} | grep main_credentials_path | sed 's|.*, "||1' | sed 's|" }.$||1')"
 
-if [ -z "${MAIN_CREDENTIALS_PATH}" ]; then
+if [ -z "${main_credentials_path}" ]; then
 
-	echo "  Error, no MAIN_CREDENTIALS_PATH variable defined in environment file (${env_file}) found." 1>&2
+	echo "  Error, no main_credentials_path variable defined in environment file (${env_file}) found." 1>&2
 	exit 6
 
 fi
 
 
-unlocked_file="${MAIN_CREDENTIALS_PATH}.dat"
-locked_file="${MAIN_CREDENTIALS_PATH}"
+unlocked_file="${main_credentials_path}.dat"
+locked_file="${main_credentials_path}"
 
 if [ ! -f "${unlocked_file}" ]; then
 
 	if [ -f "${locked_file}" ]; then
 
-		echo "  Error, the credentials file (as defined in the MAIN_CREDENTIALS_PATH variable of the environment file '${env_file}') is already locked (its locked version, '${locked_file}', already exists, whereas its unlocked version, '${unlocked_file}', does not exist)." 1>&2
+		echo "  Error, the credentials file (as defined in the main_credentials_path variable of the environment file '${env_file}') is already locked (its locked version, '${locked_file}', already exists, whereas its unlocked version, '${unlocked_file}', does not exist)." 1>&2
 		exit 20
 
 	fi
 
-	echo "  Error, no credentials file (as defined in the MAIN_CREDENTIALS_PATH variable of the environment file '${env_file}') can be found (neither in a locked version, i.e. as '${locked_file}', nor in an unlocked version, i.e. as '${unlocked_file}')." 1>&2
+	echo "  Error, no credentials file (as defined in the main_credentials_path variable of the environment file '${env_file}') can be found (neither in a locked version, i.e. as '${locked_file}', nor in an unlocked version, i.e. as '${unlocked_file}')." 1>&2
 
 	exit 21
 
@@ -73,7 +84,7 @@ fi
 
 if [ -f "${locked_file}" ]; then
 
-	echo "  Error, the credentials file (as defined in the MAIN_CREDENTIALS_PATH variable of the environment file '${env_file}') exists both in its locked version ('${locked_file}') and in its unlocked version ('${unlocked_file}'), this is abnormal." 1>&2
+	echo "  Error, the credentials file (as defined in the main_credentials_path variable of the environment file '${env_file}') exists both in its locked version ('${locked_file}') and in its unlocked version ('${unlocked_file}'), this is abnormal." 1>&2
 
 	exit 22
 
@@ -85,19 +96,12 @@ fi
 #echo "Locking credentials: from ${unlocked_file} to ${locked_file}."
 
 
-$crypt_tool -c --cipher-algo=AES256 --output ${locked_file} ${unlocked_file} 1>/dev/null
-
-res="$?"
-
-if [ $res -eq 0 ]; then
+if ${crypt_tool} -c --cipher-algo=AES256 --output "${locked_file}" "${unlocked_file}" 1>/dev/null; then
 
 	echo "(credentials locked in ${locked_file})"
-	${shred_tool} --force --remove --zero "${unlocked_file}"
-	res="$?"
+	if ! ${shred_tool} --force --remove --zero "${unlocked_file}"; then
 
-	if [ ! $res -eq 0 ]; then
-
-		echo "Error, shredding of '${unlocked_file}' failed (code: $res), removing it." 1>&2
+		echo "Error, shredding of '${unlocked_file}' failed, removing it." 1>&2
 		/bin/rm -f "${unlocked_file}"
 
 		exit 10
@@ -106,7 +110,7 @@ if [ $res -eq 0 ]; then
 
 else
 
-	echo "  Error, locking failed (code: $res), stopping, unlocked file '${unlocked_file}' left as it is." 1>&2
+	echo "  Error, locking failed, stopping, unlocked file '${unlocked_file}' left as it is." 1>&2
 
 	exit 11
 
