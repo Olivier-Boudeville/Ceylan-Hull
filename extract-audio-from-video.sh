26f22ddcd81fe4a8ee9de2428a15442bcf38182b
Olivier Boudeville <olivier.boudeville@esperide.com>
2021-09-20 21:00:27 +0200

    Robustified.

--- extract-audio-from-video.sh
+++ extract-audio-from-video.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
 
-
-usage="Usage: $(basename $0) MP4_FILE: strips the video information from specified MP4 file to generate a pure audio file (.ogg) out of it (original MP4 file not modified); useful as smaller and less resource-demanding to playback."
+usage="Usage: $(basename $0) MP4_FILE: strips the video information from specified MP4 file to generate a pure audio file (.ogg) out of it (original MP4 file not modified); useful, as smaller and less resource-demanding to playback."
 
 
 # Same content being playback, knowing the actual CPU time used by a process is
 # user+sys:
 #
-# mp4: real 188,214	user 124,142	sys 63,560	pcpu 99,73
-# ogg: real 188,258	user 1,340		sys 0,686	pcpu 1,07
+# mp4: real 188,214 user 124,142    sys 63,560  pcpu 99,73
+# ogg: real 188,258 user 1,340      sys 0,686   pcpu 1,07
 #
 # 124+63 = 187 vs 2, so ~93 times more demanding as mp4, and thus such
 # extraction is worthwhile.
 
 encoder_name="ffmpeg"
 
-encoder=$(which ${encoder_name} 2>/dev/null)
+encoder="$(which ${encoder_name} 2>/dev/null)"
 
 if [ ! -x "${encoder}" ]; then
 
@@ -28,6 +27,16 @@ fi
 
 video_file="$1"
 
+if [ -z "${video_file}" ]; then
+
+	echo "  Error, no file specified.
+${usage}" 1>&2
+
+	exit 5
+
+fi
+
+
 if [ ! -f "${video_file}" ]; then
 
 	echo "  Error, input file '${video_file}' not found." 1>&2
@@ -36,7 +45,7 @@ if [ ! -f "${video_file}" ]; then
 
 fi
 
-audio_file=$(echo "${video_file}" | sed 's|\.mp4|.ogg|1')
+audio_file="$(echo "${video_file}" | sed 's|\.mp4|.ogg|1')"
 
 echo " Generating from video file '${video_file}' following audio one: '${audio_file}'..."
 
@@ -45,18 +54,16 @@ echo " Generating from video file '${video_file}' following audio one: '${audio_
 # -y: overwrite output files without asking.
 # -aq: set the audio quality; http://wiki.hydrogenaud.io/index.php?title=Recommended_Ogg_Vorbis#Recommended_Encoder_Settings
 #
-${encoder} -i "${video_file}" -vn -acodec libvorbis -aq 5 -y -loglevel warning "${audio_file}"
-
-if [ $? -eq 0 ]; then
+if ${encoder} -i "${video_file}" -vn -acodec libvorbis -aq 5 -y -loglevel warning "${audio_file}"; then
 
 	echo
-	echo "Generation of '${audio_file}' successful."
+	echo "  Generation of '${audio_file}' is successful."
 
 else
 
 	echo
-	echo "Generation of '${audio_file}' failed!" 1>&2
+	echo "  Generation of '${audio_file}' failed!" 1>&2
 
-	exit 10
+	exit 15
 
 fi
