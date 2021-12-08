0f9e4f89dc5eff00ac9247bcb40707b60dc1faa8
Olivier Boudeville <olivier.boudeville@esperide.com>
2021-12-08 12:17:03 +0100

    Improved and restyled.

--- ip-scan.sh
+++ ip-scan.sh
@@ -1,60 +1,71 @@
 #!/bin/sh
 
-usage="Usage: $(basename $0) [PREFIX=XXX.YYY]
-Scans all IPs with PREFIX, searching for ICMP ping answers (useful to locate some devices in a local network).
-Ex: $(basename $0) 192.168
+default_prefix="192.168"
+
+usage="Usage: $(basename $0) [-h|--help] [IP_PREFIX=XXX.YYY]
+Scans all IPs with IP_PREFIX (default one being ${default_prefix}), searching for ICMP ping answers (useful to locate some devices in a local network).
+Ex: $(basename $0) 192.100
 "
 
 if [ $# -ge 2 ]; then
 
-	echo -e  "${usage}" 1>&2
+	echo "
+${usage}" 1>&2
+
 	exit 5
 
 fi
 
+if [ "$1" = "-h" ] || [ "$1" = "-h" ]; then
 
-PREFIX="$1"
+	echo "${usage}"
 
-if [ -z "$PREFIX" ]; then
-	PREFIX="192.168"
+	exit 0
+
 fi
 
-LOG_FILE="ip-scan.log"
+prefix="$1"
+
+if [ -z "${prefix}" ]; then
+	prefix="192.168"
+fi
+
+log_file="ip-scan.log"
 
 
 output_message()
 {
 
-	echo $* >> $LOG_FILE
+	echo $* >> ${log_file}
 	echo $*
 
 }
 
-output_message "Searching ICMP-responding IP addresses in prefix '$PREFIX' at "`date`
+output_message "Searching ICMP-responding IP addresses in prefix '${prefix}' at $(date)"
 output_message
 
-A=0
-B=1
+a=0
+b=1
 
-while [ $A -le 255 ]; do
+while [ ${a} -le 255 ]; do
 
-	echo " - exploring range $PREFIX.$A.1-255"
+	echo " - exploring range ${prefix}.${a}.1-255"
 
-	while [ $B -le 255 ]; do
-		#echo "A=$A, B=$B"
-		TARGET="$PREFIX.$A.$B"
-		#echo "Testing $TARGET"
-		ping $TARGET -c 1 1>/dev/null
-		if [ $? -eq 0 ]; then
-			output_message "++++ Found $TARGET!!!!!"
+	while [ ${b} -le 255 ]; do
+		#echo "a=${a}, b=${b}"
+		target="${prefix}.${a}.${b}"
+		#echo "Testing ${target}"
+		if ping ${target} -c 1 1>/dev/null; then
+			output_message "++++ Found ${target}!!!!!"
 		else
-			output_message "(nothing at $TARGET)"
+			output_message "(nothing at ${target})"
 		fi
-		B=`expr $B + 1`
+		b=$(expr ${b} + 1)
 	done
 
-	A=`expr $A + 1`
-	B=1
+	a=$(expr ${a} + 1)
+	b=1
+
 done
 
 output_message "End of search at $(date)"
