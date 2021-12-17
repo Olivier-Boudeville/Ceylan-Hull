a5f61bdcc8e0af2a299ac93f495d1562f49d69c0
Olivier Boudeville <olivier.boudeville@esperide.com>
2021-12-17 16:51:30 +0100

    Usage clarified.

--- install-arch-package.sh
+++ install-arch-package.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-usage="Usage: $(basename $0) [-h|--help] PACKAGE_NAME: installs a package on Arch Linux, found as either a standard Arch one (with pacman), or as an AUR package.
+usage="Usage: $(basename $0) [-h|--help] PACKAGE_NAME: installs a package on Arch Linux, found as either a standard Arch one (with pacman), or as an AUR package; any needed prior installation or update of an AUR installer is managed automatically.
 
 To be run as a non-priviledged user (sudo used whenever necessary)."
 
