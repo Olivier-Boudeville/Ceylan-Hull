332d7eff13659d171624f061f7b13378127c8312
Olivier Boudeville <olivier.boudeville@esperide.com>
2021-12-18 22:16:42 +0100

    Hint added.

--- highlight.sh
+++ highlight.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-usage="$(basename $0) [FILENAME] [-i|--ignore-case] PATTERNS: highlights, in specified data (either the specified file or the standard input, when used through a pipe), the specified pattern(s), in a case-insensitive manner if requested.\n\n  Examples:\n    $(basename $0) my_file.txt dog wolf\n    echo \"aaaBBaaccc\" | $(basename $0) -i bb c\n"
+usage="$(basename $0) [FILENAME] [-i|--ignore-case] PATTERNS: highlights, in specified data (either the specified file or the standard input, when used through a pipe), the specified pattern(s), in a case-insensitive manner if requested.\n\n  Examples:\n    $(basename $0) my_file.txt dog wolf\n    echo \"aaaBBaaccc\" | $(basename $0) -i bb c\n
+
+Note that such highlighting should preferably be done last in a series of pipes, as the color coding may interfere for example with the matching performed by any subsequent grep.
+"
 
 if [ $# -eq 0 ] || [ $1 = "-h" ] || [ $1 = "--help" ]; then
 
