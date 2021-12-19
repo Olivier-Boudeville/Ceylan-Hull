ec7155a2ff32eaefdf29d7c09e94e26ee5c1a2ba
Olivier Boudeville <olivier.boudeville@esperide.com>
2021-12-19 13:52:06 +0100

    Minor update.

--- display-opengl-information.sh
+++ display-opengl-information.sh
@@ -9,18 +9,18 @@ echo
 DISPLAY=:0
 
 echo "  * Hardware-accelerated rendering:"
-GLXINFO=$(which glxinfo 2>/dev/null)
+glxinfo="$(which glxinfo 2>/dev/null)"
 
 
-if [ ! -x "$GLXINFO" ]; then
+if [ ! -x "${glxinfo}" ]; then
 
-	echo "  Error, glxinfo tool not found. Run for example: 'yaourt glxinfo'" 1>&2
+	echo "  Error, glxinfo tool not found. Run for example: 'install-arch-package.sh glxinfo'." 1>&2
 	exit 5
 
 fi
 
-$GLXINFO | egrep "direct rendering"
-$GLXINFO | egrep "OpenGL .* string"
+${glxinfo} | egrep "direct rendering"
+${glxinfo} | egrep "OpenGL .* string"
 
 echo
 
@@ -37,4 +37,4 @@ echo "  * AIGLX status: "
 grep AIGLX /var/log/Xorg.0.log | grep -v '(WW)'
 echo
 
-echo "..done"
+echo "...done"
