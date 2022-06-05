318abcb8728c2c983fdf32501c72d830661354f6
Olivier Boudeville <olivier.boudeville@esperide.com>
2022-06-05 23:07:06 +0200

    Enriched.

--- manage-wifi.sh
+++ manage-wifi.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-usage="Usage: $(basename $0) [ status | start | scan | stop | isolate | --help | -h ]: manages the wifi configuration
+usage="Usage: $(basename $0) [--help|-h] [status|start|scan|stop|isolate]: manages the wifi configuration
   - without argument or with 'status': returns status
   - with 'start': put the (guessed) wifi interface up
   - with 'scan': scan for wifi access points (once started)
@@ -23,10 +23,10 @@ if [ ! $(id -u) -eq 0 ]; then
 fi
 
 
-IW=$(which iw 2>/dev/null)
+iw="$(which iw 2>/dev/null)"
 
 
-if [ ! -x "$IW" ]; then
+if [ ! -x "${iw}" ]; then
 
 	echo "  Error, no available 'iw' tool (use 'pacman -S iw'?)." 1>&2
 	exit 10
@@ -34,9 +34,9 @@ if [ ! -x "$IW" ]; then
 fi
 
 
-IP=$(which ip 2>/dev/null)
+ip="$(which ip 2>/dev/null)"
 
-if [ ! -x "$IP" ]; then
+if [ ! -x "${ip}" ]; then
 
 	echo "  Error, no available 'ip' tool (use 'pacman -S ip'?)." 1>&2
 	exit 11
@@ -44,10 +44,10 @@ if [ ! -x "$IP" ]; then
 fi
 
 
-RFKILL=$(which rfkill 2>/dev/null)
+rfkill="$(which rfkill 2>/dev/null)"
 
 
-if [ ! -x "$RFKILL" ]; then
+if [ ! -x "${rfkill}" ]; then
 
 	echo "  Error, no available 'rfkill' tool (use 'pacman -S rfkill'?)." 1>&2
 	exit 12
@@ -55,59 +55,71 @@ if [ ! -x "$RFKILL" ]; then
 fi
 
 
-IF=$($IW dev|grep Interface | sed 's|.*Interface ||')
+if="$(${iw} dev | grep Interface | sed 's|.*Interface ||')"
 
-if [ -n "$IF" ]; then
+if [ -n "${if}" ]; then
 
-	echo "Guessed wifi interface: $IF"
+	echo "Guessed wifi interface: ${if}"
 
 else
 
-	IF="wlo1"
-	echo "Unable to guess wifi interface, assuming $IF."
+	if="wlo1"
+	echo "Unable to guess wifi interface, assuming ${if}."
 
 fi
 
 
 arg="$1"
 
-if [ -z "$arg" ] || [ "$arg" = "status" ]; then
+if [ -z "${arg}" ] || [ "${arg}" = "status" ]; then
 
-	$RFKILL list
-	echo "Status of wifi interface $IF: "$($IW dev $IF link)
+	${rfkill} list
+	echo "Status of wifi interface ${if}: $(${iw} dev ${if} link)"
 
-elif [ "$arg" = "start" ]; then
+elif [ "${arg}" = "start" ]; then
 
-	$RFKILL unblock wifi
+	${rfkill} unblock wifi
 
 	# Disabled as it resulted in netctl failing, expecting the interface to be
 	# down:
-	#echo "Setting wifi interface $IF up."
-	#$IP link set $IF up
+	#echo "Setting wifi interface ${if} up."
+	#${ip} link set ${if} up
 
-	#$IW dev $IF link
+	#${iw} dev ${if} link
 
-elif [ "$arg" = "stop" ]; then
+elif [ "${arg}" = "stop" ]; then
 
-	echo "Setting wifi interface $IF down."
-	$IP link set $IF down
-	$RFKILL block wifi
+	echo "Setting wifi interface ${if} down."
+	${ip} link set "${if}" down
+	${rfkill} block wifi
 
-	#$IW dev $IF link
+	#${iw} dev ${if} link
 
-elif [ "$arg" = "scan" ]; then
+elif [ "${arg}" = "scan" ]; then
 
-	echo "Scanning for wifi networks with interface $IF, found following SSIDs:"
-	$IW dev $IF scan | grep SSID | sort | uniq | sed 's|.*SSID: | - |1'
 
-elif [ "$arg" = "isolate" ]; then
+	# If the wifi interface is reported as blocked, one may use for example:
+	# 'rfkill unblock 1'.
+
+	# Otherwise an error "command failed: Network is down (-100)" is triggered:
+	echo "Setting wifi interface ${if} up."
+	${ip} link set "${if}" up
+
+	echo "Scanning for wifi networks with interface ${if}, found following SSIDs:"
+	${iw} dev "${if}" scan | grep 'SSID: ' | sort | uniq | sed 's|.*SSID: | - |1'
+
+	# Expected by netctl:
+	echo "Setting wifi interface ${if} down."
+	${ip} link set "${if}" down
+
+elif [ "${arg}" = "isolate" ]; then
 
 	echo "Blocking all wireless accesses now."
-	$RFKILL block all
+	${rfkill} block all
 
 else
 
-	echo "  Error, invalid argument ('$arg')." 1>&2
+	echo "  Error, invalid argument ('${arg}')." 1>&2
 	exit 15
 
 fi
@@ -115,7 +127,7 @@ fi
 
 # To connect:
 
-# First, ensure that RFKILL is disabled:
+# First, ensure that rfkill is disabled:
 # We must have:
 # rfkill list wifi
 #0: phy0: Wireless LAN
@@ -123,13 +135,15 @@ fi
 #	Hard blocked: no
 #
 # If hard blocked, toggle an hardware button.
-# If soft blocked, push once the wireless key (F12)
-# Then re-check.
 #
+# If soft blocked, push once the wireless key (ex: F8 or F12, or 'rfkill unblock
+# 1')
+#
+# Then re-check.
 
 
-# - if no encryption: 'iw dev $IF connect $ESSID'
-# - if WEP is used: 'iw dev $IF connect $ESSID key 0:$KEY'
+# - if no encryption: 'iw dev ${if} connect $ESSID'
+# - if WEP is used: 'iw dev ${if} connect $ESSID key 0:$KEY'
 # - if using WPA/WPA2:
 
 # netctl start "wlo1-100_Fils_d'Ariane"
@@ -139,20 +153,17 @@ fi
 # netctl start "wlo1-100_Fils_d'Ariane"
 
 # Otherwise:
-# 'wpa_supplicant -i $IF -c < ( wpa_passphrase $ESSID $KEY )'
+# 'wpa_supplicant -i ${if} -c < ( wpa_passphrase $ESSID $KEY )'
 
 # Check with 'status'.
 
 # Then:
-#
-# - either 'dhcpcd $IF'
+# - either 'dhcpcd ${if}'
 # - or, for example:
-
-# ip addr add 192.168.0.2/24 dev $IF
+# ip addr add 192.168.0.2/24 dev ${if}
 # ip route add default via 192.168.0.1
 
 # One may also use:
-
 # - wifi-radar
 # - wifi-menu
 # - rfkill
