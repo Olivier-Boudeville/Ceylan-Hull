aa856ef3339d1ac1ed93c2ec2db8a0dee2d5f977
Olivier Boudeville <ceylan-commits@esperide.com>
2022-07-19 12:53:03 +0200

    updatedb integrated.

--- update-locate-database.sh
+++ update-locate-database.sh
@@ -2,27 +2,43 @@
 
 usage="Usage: $(basename $0): updates the 'locate' database, for faster look-ups in filesystems."
 
-SLOCATE_EXE=/usr/bin/slocate
-SLOCATE_CONF=/etc/updatedb.conf
+
+updatedb_exec=$(which updatedb 2>/dev/null)
+
+slocate_exec=/usr/bin/slocate
+slocate_conf=/etc/updatedb.conf
+
 
 if [ ! $(id -u) -eq 0 ]; then
 	echo "Error, only root can do that."
 	exit 1
 fi
 
-if [ -x "$SLOCATE_EXE" ]; then
+
+# Apparently the best approach now:
+if [ -x "${updatedb_exec}" ]; then
+
+	"${updatedb_exec}"
+
+	exit
+
+fi
+
+
+# Otherwise:
+if [ -x "${slocate_exec}" ]; then
 
 	echo "Updating locate database..."
 
-	if [ -f "$SLOCATE_CONF" ]; then
-		source $SLOCATE_CONF
-		$SLOCATE_EXE -u && echo "... done"
+	if [ -f "${slocate_conf}" ]; then
+		source ${slocate_conf}
+		${slocate_exec} -u && echo "... done"
 	else
-		$SLOCATE_EXE -f proc -u && echo "... done"
+		${slocate_exec} -f proc -u && echo "... done"
 	fi
 	chown root.slocate /var/lib/slocate/slocate.db
 
 else
-	echo "Error, no slocate tool found (no $SLOCATE_EXE)."
+	echo "Error, no slocate tool found (no ${slocate_exec})."
 	exit
 fi
