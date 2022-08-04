6d559e1f73a9f96ce6a4adf950cd9e99dac549dc
Olivier Boudeville <olivier.boudeville@esperide.com>
2022-08-04 15:32:58 +0200

    Help option added.

--- fix-whitespaces-in-tree.sh
+++ fix-whitespaces-in-tree.sh
@@ -8,11 +8,18 @@ target_extensions="txt markdown md asciidoc spec erl hrl escript app edoc rst sr
 target_filenames="Makefile rebar.config .gitignore LICENSE"
 
 
-usage="Usage: $(basename $0) ROOT_DIRECTORY: fixes whitespace problems in all eligible files found from the specified root directory tree.
+usage="Usage: $(basename $0) [-h|--help] ROOT_DIRECTORY: fixes whitespace problems in all eligible files found from the specified root directory tree.
 Useful, when operating on a fork, to properly format files once for all and commit the result so that these formatting changes remain cleanly and clearly separated from the upcoming meaningful others.
 Target extensions are: '${target_extensions}'.
 Target filenames are: '${target_filenames}'.
 "
+
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+
+	echo "${usage}"
+	exit
+
+fi
 
 target_dir="$1"
 
