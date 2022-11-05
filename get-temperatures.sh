f5f6650c3c8299f71273840e253e39199fb4de3e
Olivier Boudeville <olivier.boudeville@esperide.com>
2022-11-05 09:43:43 +0100

    Better filtering.

--- get-temperatures.sh
+++ get-temperatures.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 usage="Usage: $(basename $0): returns a list of the temperatures known of the system, for various components (cores, disks, motherboard, etc.)
 "
 
@@ -23,9 +22,13 @@ if [ ! -x "${sensors_cmd}" ]; then
 
 fi
 
-# Removing "(high = +100.0°C, crit = +100.0°C)" or "(crit = +128.0°C)", as well
-# as blank lines or non-relevant sensors:
+
+# Some sensors are almost random generators.
+
+# Removing "(high = +100.0°C, crit = +100.0°C)" or "(crit = +128.0°C)", "ALARM
+# sensor = CPU diode", as well as blank lines or non-relevant sensors (negative
+# or null temperatures, 127.* °C ones, etc.:
 #
 # (not relevant enough: grep -E '(°C|Adapter)')
 #
-"${sensors_cmd}" | grep '°C' | sed 's|(.*)||g' | sed '/^[[:space:]]*$/d' | grep -v '+0.0°C'
+"${sensors_cmd}" | grep '°C' | sed 's|(.*)||g' | sed '/^[[:space:]]*$/d' | sed 's|°C.*$|°C|1' | grep -v '+0.0°C' | grep -vE '\-[0-9]*\.[0-9]*°C' | grep -vE '\+127\..*°C'
