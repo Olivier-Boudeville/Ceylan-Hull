6dffb2df207b73bc09ff443ce55422adc7f79d8c
Olivier Boudeville <olivier.boudeville@esperide.com>
2023-05-13 10:42:49 +0200

    Improved option order.

--- highlight.sh
+++ highlight.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-usage="$(basename $0) [FILENAME] [-i|--ignore-case] PATTERNS: highlights, in specified data (either the specified file or the standard input, when used through a pipe), the specified pattern(s), in a case-insensitive manner if requested.\n\n  Examples:\n    $(basename $0) my_file.txt dog wolf\n    echo \"aaaBBaaccc\" | $(basename $0) -i bb c\n
+usage="$(basename $0) [-i|--ignore-case] [FILENAME] PATTERNS: highlights, in the specified data (either the specified file or the standard input, when used through a pipe), the specified pattern(s), in a case-insensitive manner if requested.\n\n  Examples:\n    $(basename $0) my_file.txt dog wolf\n    echo \"aaaBBaaccc\" | $(basename $0) -i bb c\n
 
 Note that such highlighting should preferably be done last in a series of pipes, as the color coding may interfere for example with the matching performed by any subsequent grep.
 "
@@ -14,12 +14,15 @@ fi
 
 case_opt=""
 
+
 if [ $1 = "-i" ] || [ $1 = "--ignore-case" ]; then
 
-	case_opt="$1"
+	#echo "(ignoring case)"
+	case_opt="--ignore-case"
 	shift
 
 fi
+
 
 if [ $# -ge 1 -a -f "$1" ]; then
 
@@ -31,8 +34,8 @@ if [ $# -ge 1 -a -f "$1" ]; then
 
 	#if [ ! -f "${input}" ]; then
 	#
-	#	echo "  '${input}': file not found." 1>&2
-	#	exit 10
+	#   echo "  '${input}': file not found." 1>&2
+	#   exit 10
 	#
 	#fi
 
@@ -55,6 +58,7 @@ patterns=$(echo ${raw_patterns} | sed 's| |\\\||g')
 #echo "- patterns: ${patterns}"
 
 # 'always' allows to pipe with a color-enabled pager afterwards:
-# (ex: with 'less -r -X')
+# (e.g. with 'less -r -X')
 #
+#echo grep ${case_opt} --color=always "${patterns}\|$" "${input}"
 grep ${case_opt} --color=always "${patterns}\|$" "${input}"
