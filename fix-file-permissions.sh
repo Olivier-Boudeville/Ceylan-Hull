5bad6b10f817ee1d102682e1968a7b680a983aec
Olivier Boudeville <olivier.boudeville@esperide.com>
2023-09-12 15:37:50 +0200

    More fixes.

--- fix-file-permissions.sh
+++ fix-file-permissions.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-usage="Usage: $(basename $0): corrects the UNIX permissions for the most common file extensions, for all files in the current directory."
+usage="Usage: $(basename $0): corrects the filesystem permissions for the most common file extensions, for all files in the current directory (non-recursively)."
 
 
-echo "Correcting file permissions in $(pwd)..."
+echo "Correcting the file permissions in $(pwd)..."
 
-for f in *.doc *.docx *.xls *.xlsx *.ppt *.pptx *.pdf *.txt *.zip *.png *.jpg *.jpeg *.gz *.tgz *.bz2 *.xz *.rst *.mp4 *.gpg ; do
+for f in *.doc *.docx *.xls *.xlsx *.ppt *.pptx *.pdf *.txt *.zip *.png *.jpg *.jpeg *.gz *.tgz *.bz2 *.xz *.rst *.gif *.png *.jpeg *.mp4 *.gpg *.cxx *.hxx *.erl; do
 
 	if [ -f "$f" ]; then
 
