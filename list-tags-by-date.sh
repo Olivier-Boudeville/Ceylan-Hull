c9fc4254064a91f75b16665a1336e4e561c3ca7b
Olivier Boudeville <olivier.boudeville@esperide.com>
2023-10-17 17:02:33 +0200

    Better command used.

--- list-tags-by-date.sh
+++ list-tags-by-date.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-usage="Usage: $(basename $0) [-h|--help]: lists, for the current VCS (GIT) repository, all (annotated) tags, from the oldest one to the latest one."
+usage="Usage: $(basename $0) [-h|--help]: lists, for the current VCS (Git) repository, all (annotated) tags, from the oldest one to the latest one.
+
+For example:
+ Tag refs/tags/debug.1.0.0 was set on Thu Jul 6 13:24:01 2023 +0200
+ Tag refs/tags/some_fix was set on Mon Sep 25 18:11:23 2023 +0200
+"
 
 
 if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
@@ -13,4 +18,6 @@ fi
 echo "  Listing repository tags, from oldest to newest:"
 
 # Removing empty lines:
-git for-each-ref --sort=taggerdate --format '%(tag)' | sed '/^[[:space:]]*$/d'
+#git for-each-ref --sort=taggerdate --format '%(tag)' | sed '/^[[:space:]]*$/d'
+
+git for-each-ref --sort=creatordate --format 'Tag %(refname) was set on %(creatordate)' refs/tags
