64d23fa1edf3a75a8e8350f28f3b3e3022ff7d65
Olivier Boudeville <olivier.boudeville@esperide.com>
2024-03-24 22:00:06 +0100

    Robustified.

--- set-audio-volume.sh
+++ set-audio-volume.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 
-usage="Usage: $(basename $0) [[TARGET_AUDIO_SINK] NEW_PERCENTAGE_VOLUME]
-  Sets the volume (as a percentage of the base maximum one - possibly going over 100%) either for the specified audio sink or for any automatically detected one (currently running sink, otherwise idle, otherwise suspended).
+usage="Usage: $(basename $0) [-v] [[TARGET_AUDIO_SINK] NEW_PERCENTAGE_VOLUME]
+  Sets the volume (as a percentage of the base maximum one - possibly going over 100%) either for the specified audio sink or for any automatically detected one (currently first running sink, otherwise first idle, otherwise first suspended).
+
+  Option: -v enables the verbose mode
 
   If executed with no argument, returns the current volume.
 
@@ -17,10 +19,20 @@ usage="Usage: $(basename $0) [[TARGET_AUDIO_SINK] NEW_PERCENTAGE_VOLUME]
 # Otherwise just get the current volume:
 do_set=0
 
+is_verbose=1
+
+if [ "$1" = "-v" ]; then
+
+	shift
+	is_verbose=0
+	[ $is_verbose -eq 0 ] && echo "Verbose mode enabled."
+
+fi
 
 
-
-# Returned in the target_sink variable:
+# Returned in the target_sink variable (corresponding to the numerical index of
+# the audio sink of interest, like O, 1 etc.):
+#
 detect_audio_sink()
 {
 
@@ -38,22 +50,28 @@ detect_audio_sink()
 	# Number of context lines to return before the current state:
 	line_context_count=4
 
-	#echo "(checking running sinks)"
+	[ $is_verbose -eq 0 ] && echo "(checking running sinks)"
 	sink_type="running"
-	target_sink="$(pacmd list-sinks | grep -B ${line_context_count} RUNNING | grep index | awk ' { print $NF } ')"
+
+	# Should multiple sinks match, we select the first one.
+	#
+	# NF is the number of fields of the current record, $NF is thus the value of
+	# the last field:
+	#
+	target_sink="$(pacmd list-sinks | grep -B ${line_context_count} RUNNING | grep index | head -1 | awk ' { print $NF } ')"
 
 
 	if [ -z "${target_sink}" ]; then
 
-		#echo "(checking idle sinks)"
+		[ $is_verbose -eq 0 ] && echo "(checking idle sinks)"
 		sink_type="idle"
-		target_sink="$(${pacmd} list-sinks | grep -B ${line_context_count} IDLE | grep index | awk ' { print $NF } ')"
+		target_sink="$(${pacmd} list-sinks | grep -B ${line_context_count} IDLE | grep index | head -1 | awk ' { print $NF } ')"
 
 		if [ -z "${target_sink}" ]; then
 
-			#echo "(checking suspended sinks)"
+			[ $is_verbose -eq 0 ] && echo "(checking suspended sinks)"
 			sink_type="suspended"
-			target_sink="$(${pacmd} list-sinks | grep -B ${line_context_count} SUSPENDED | grep index | awk ' { print $NF } ')"
+			target_sink="$(${pacmd} list-sinks | grep -B ${line_context_count} SUSPENDED | grep index | head -1 | awk ' { print $NF } ')"
 
 			if [ -z "${target_sink}" ]; then
 
@@ -81,7 +99,7 @@ detect_audio_sink()
 
 	fi
 
-	#echo "  Auto-detected sink: ${target_sink}."
+	[ $is_verbose -eq 0 ] && echo "  Auto-detected sink: ${target_sink}."
 
 }
 
@@ -147,7 +165,7 @@ fi
 # Always useful to report:
 echo "  The current volume of the ${sink_type} audio sink #${target_sink} is:"
 
-if ! "${pactl}" -- get-sink-volume ${target_sink} | grep Volume; then
+if ! "${pactl}" -- get-sink-volume "${target_sink}" | grep Volume; then
 
 	echo "  Error, failed to read the volume for sink #${target_sink}." 1>&2
 
@@ -160,7 +178,7 @@ if [ $do_set -eq 0 ]; then
 
 	echo "  Setting volume to ${target_volume}% for ${sink_type} audio sink #${target_sink}."
 
-	if ! "${pactl}" -- set-sink-volume ${target_sink} "${target_volume}%"; then
+	if ! "${pactl}" -- set-sink-volume "${target_sink}" "${target_volume}%"; then
 
 		echo "  Error, failed to modify the volume for sink #${target_sink}." 1>&2
 
