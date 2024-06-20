0b8d8ab3b9d02860bd1a1234c5cd738dedddd62f
Olivier Boudeville <olivier.boudeville@esperide.com>
2024-06-20 23:09:16 +0200

    convert-from-ISO-8859-to-utf8.sh added.

---
+++ convert-from-ISO-8859-to-utf8.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+
+# Source format needed, otherwise 'iconv: illegal input sequence at position
+# XXX':
+#
+#source_format="ISO-8859-1"
+source_format="ASCII"
+
+target_format="utf-8"
+
+usage="Usage: $(basename $0) [-h|--help] TEXT_FILE: converts (in-place) the specified (text) file from a ${source_format} format to a ${target_format} one."
+
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+
+	echo "${usage}"
+	exit
+
+fi
+
+
+
+iconv="$(which iconv 2>/dev/null)"
+
+if [ ! -x "${iconv}" ]; then
+
+	echo "  Error, no 'iconv' executable available." 1>&2
+
+	exit 5
+
+fi
+
+
+
+if [ -z "$1" ]; then
+
+	echo "  Error, no argument specified.
+${usage}" 1>&2
+
+	exit 10
+
+fi
+
+
+
+source_file="$1"
+
+if [ ! -f "${source_file}" ]; then
+
+	echo "  Error, file '${source_file}' does not exist." 1>&2
+
+	exit 15
+
+fi
+
+shift
+
+
+if [ -n "$*" ]; then
+
+	echo "  Error, extra arguments specified ('$*')." 1>&2
+
+	exit 25
+
+fi
+
+
+
+tmp_file="${source_file}.tmp"
+
+# No '-f' intentionally:
+
+/bin/mv "${source_file}" "${tmp_file}" && iconv -f "${source_format}"  -t "${target_format}" "${tmp_file}" > "${source_file}" && echo "File '${source_file}' converted!"
