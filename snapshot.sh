443ba7420aac566ee7ad1fdfca0c367f6ce67331
Olivier Boudeville <olivier.boudeville@esperide.com>
2024-09-04 22:41:33 +0200

    Now supporting excludes.

--- snapshot.sh
+++ snapshot.sh
@@ -4,8 +4,11 @@ resulting_ext="tar.xz.gpg"
 
 date_prefix="$(date "+%Y%m%d")"
 
-usage="Usage: $(basename $0) [-h|--help] DIRECTORY_TREE_TO_SNAPSHOT: performs a snapshot of the specified filesystem tree, i.e. creates a corresponding timestamped ${resulting_ext} archive.
-  Example: '$(basename $0) osdl' will produce a ${date_prefix}-osdl-snapshot.${resulting_ext} archive in the current directory."
+usage="Usage: $(basename $0) [-h|--help] [-e|--exclude EXCLUDED_ENTRY] DIRECTORY_TREE_TO_SNAPSHOT: performs a snapshot of the specified filesystem tree, i.e. creates a corresponding timestamped ${resulting_ext} archive.
+
+  Any number of excludes, relative to the target directory, can be specified.
+
+  Example: '$(basename $0) -e foo -e \"**/bar/baz\" osdl' will produce a ${date_prefix}-osdl-snapshot.${resulting_ext} archive of the 'osdl' tree in the current directory, excluding osdl/foo and osdl/**/bar/baz."
 
 
 crypt_name="crypt.sh"
@@ -14,27 +17,11 @@ crypt_tool="$(which ${crypt_name} 2>/dev/null)"
 
 if [ ! -x "${crypt_tool}" ]; then
 
-	echo "    Error, no executable crypt tool (${crypt_name}) found." 1>&2
+	echo "  Error, no executable crypt tool ('${crypt_name}') found." 1>&2
 	exit 4
 
 fi
 
-
-if [ ! $# -eq 1 ]; then
-
-	echo "    Error, exactly one parameter expected.
-${usage}" 1>&2
-	exit 5
-
-fi
-
-if [ ! $# -eq 1 ]; then
-
-	echo "    Error, exactly one parameter expected.
-${usage}" 1>&2
-	exit 5
-
-fi
 
 if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
 
@@ -44,11 +31,67 @@ if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
 fi
 
 
+excludes=""
+
+token_eaten=0
+
+while [ $token_eaten -eq 0 ]; do
+
+	token_eaten=1
+
+	if [ "$1" = "-e" ] || [ "$1" = "--exclude" ]; then
+
+		shift
+
+		if [ -z "$1" ]; then
+
+			echo "  Error, exclude option specified, but no element could be found after it." 1>&2
+
+			exit 10
+
+		fi
+
+		# Do not ever type to protect $1 with ' or \", tar would take them
+		# literally:
+		#
+		excludes="${excludes} --exclude=$1"
+
+		shift
+
+		token_eaten=0
+
+	fi
+
+done
+
+
 target_dir="$1"
+
+if [ -z "${target_dir}" ]; then
+
+	echo "  Error, no directory to snapshot specified.
+${usage}" 1>&2
+
+	exit 15
+
+fi
+
+shift
+
+
+if [ ! $# -eq 0 ]; then
+
+	echo " Error, extra parameters specified: $*.
+${usage}" 1>&2
+
+	exit 20
+
+fi
+
 
 if [ ! -d "${target_dir}" ]; then
 
-	echo "    Error, the specified directory ('${target_dir}') does not exist.
+	echo "  Error, the specified directory ('${target_dir}') to snapshot does not exist.
 ${usage}" 1>&2
 	exit 6
 
@@ -59,7 +102,8 @@ archive_name="${date_prefix}-$(basename ${target_dir})-snapshot.tar.xz"
 #echo "archive_name = ${archive_name}"
 
 
-# A bit of black magic about exclusions despite shell variables; only the third form works:
+# A bit of black magic about exclusions despite shell variables; only the third
+# form works:
 
 #extra_opts="--exclude={*.jpg,*.jpeg,*.JPG}"
 
@@ -67,6 +111,8 @@ archive_name="${date_prefix}-$(basename ${target_dir})-snapshot.tar.xz"
 
 # The only right one:
 #extra_opts="--exclude=*.jpg --exclude=*.jpeg --exclude=*.JPG --exclude=*.png --exclude=*.bmp --exclude=*.gif --exclude=*.bz2 --exclude=*.gz --exclude=*.zip --exclude=*.xz --exclude=*.rar --exclude=*.iso --exclude=*.gpg --exclude=*.pack --exclude=*.mp4 --exclude=*.ogg --exclude=*.backup --exclude=*.apk --exclude=*.img"
+
+extra_opts="${excludes}"
 
 
 if [ -n "${extra_opts}" ]; then
@@ -76,9 +122,10 @@ if [ -n "${extra_opts}" ]; then
 fi
 
 
+# /usr/bin/tar generally:
 if ! tar ${extra_opts} -cvJf "${archive_name}" "${target_dir}"; then
 
-	echo "    Error, archive creation failed." 1>&2
+	echo "  Error, the archive creation failed." 1>&2
 	exit 7
 
 fi
