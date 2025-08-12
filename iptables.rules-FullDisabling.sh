8087004c21cd5ab89bec384cd78ed89b4c2f9a62
Olivier Boudeville <olivier.boudeville@esperide.com>
2025-08-12 21:35:03 +0200

    Robustified.

--- iptables.rules-FullDisabling.sh
+++ iptables.rules-FullDisabling.sh
@@ -1,12 +1,44 @@
 #!/bin/sh
 
-usage="Usage: $(basename $0): disables all firewall rules."
+usage="Usage: $(basename $0): disables all firewall rules, alternatively to a measure akin to 'systemctl stop iptables.service'."
+
+
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+
+	echo "${usage}"
+
+	exit
+
+fi
+
+
+if [ -n "$1" ]; then
+
+	echo "  Error, not argument expected.
+${usage}" 1>&2
+
+	exit 5
+
+fi
+
+
+# Generally /sbin/iptables:
+iptables="$(which iptables 2>/dev/null)"
+
+if [ ! -x "${iptables}" ]; then
+
+	echo " Error, not 'iptables' executable found." 1>&2
+
+	exit 10
+
+fi
+
 
 if [ ! $(id -u) -eq 0 ]; then
 
 	echo "
-	Error, you must be root, aborting." 1>&2
-	exit 5
+	Error, you must be root; aborting." 1>&2
+	exit 15
 
 fi
 
@@ -14,20 +46,15 @@ fi
 echo "Disabling *permanently* ALL iptables rules (beware, all traffic accepted!)."
 
 
-iptables=/sbin/iptables
-
-${iptables} -F && ${iptables} -X && ${iptables} -Z && ${iptables} -F -t nat && ${iptables} -X -t nat && ${iptables} -Z -t nat && ${iptables} -P INPUT ACCEPT && ${iptables} -P FORWARD ACCEPT && ${iptables} -P OUTPUT ACCEPT
-
-
-if [ ! $? -eq 0 ] ; then
+if ! "${iptables}" -F && "${iptables}" -X && "${iptables}" -Z && "${iptables}" -F -t nat && "${iptables}" -X -t nat && "${iptables}" -Z -t nat && "${iptables}" -P INPUT ACCEPT && "${iptables}" -P FORWARD ACCEPT && "${iptables}" -P OUTPUT ACCEPT; then
 
 	echo "
-	Error, disabling failed." 1>&2
+	Error, iptables disabling failed." 1>&2
 
-	exit 15
+	exit 50
 
 else
 
-	echo "Disabling succeeded."
+	echo "Disabling of iptables succeeded. Beware!"
 
 fi
