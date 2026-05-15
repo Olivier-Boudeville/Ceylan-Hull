e240affe8397b0397e74f43faeb793b473eab5f3
Olivier Boudeville <olivier.boudeville@esperide.com>
2026-05-15 12:19:27 +0200

    CPU limited and notified now.

--- snapshot.sh
+++ snapshot.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Copyright (C) 2009-2026 Olivier Boudeville
+#
+# Author: Olivier Boudeville [olivier (dot) boudeville (at) esperide (dot) com]
+#
+# This file is part of the Ceylan-Hull toolbox (see http://hull.esperide.org).
+
+
 resulting_ext="tar.xz.gpg"
 
 date_prefix="$(date "+%Y%m%d")"
@@ -122,12 +129,58 @@ if [ -n "${extra_opts}" ]; then
 fi
 
 
-# /usr/bin/tar generally:
-if ! tar ${extra_opts} -cvJf "${archive_name}" "${target_dir}"; then
+# Compression might be quite power consuming, at the risk of overheat.
+#
+# We try to prevent that by forcing a lesser (actually: least) favorable
+# scheduling for the corresponding processes; however 'nice' has little to no
+# effect (it is only useful to enforce respective priorities between running
+# executables), compression still leads to higher temperatures; see next
+# cpulimit section.
+#
+#nice_opt=""
+nice_opt="nice --adjustment=19"
+
+
+# cpulimit allows to enforce a stricter CPU usage limit to a process.
+#
+# It may not be available on the local host (with Arch Linux, use 'pacman -Sy
+# cpulimit').
+#
+# By default no root privileges will be specifically needed.
+#
+cpulimit="$(which cpulimit 2>/dev/null)"
+
+if [ -x "${cpulimit}" ]; then
+
+	# Only 50% of one "CPU" (presumably one core - hence not a lot at all):
+	#cpu_limit_percentage=50
+
+	# Better:
+	cpu_limit_percentage=100
+
+	# For testing:
+	#cpu_limit_percentage=1
+
+	# We leave any active 'nice'.
+
+	# Add -v for a lot more information:
+	cpu_limit_expr="${cpulimit} --limit=${cpu_limit_percentage} --include-children"
+
+fi
+
+
+tar="/usr/bin/tar"
+
+if ! ${nice_opt} ${cpu_limit_expr} "${tar}" ${extra_opts} -cvJf "${archive_name}" "${target_dir}"; then
 
 	echo "  Error, the archive creation failed." 1>&2
 	exit 7
 
 fi
 
-"${crypt_tool}" "${archive_name}" && echo "Snapshot file ${archive_name}.gpg is ready!"
+bong.sh 1>/dev/null
+notify.sh "Archive created, needing a password to encrypt it now."
+bong.sh 1>/dev/null
+
+# No need to slow it down too much, yet:
+${nice_opt} ${cpu_limit_expr} "${crypt_tool}" "${archive_name}" && echo "Snapshot file ${archive_name}.gpg is ready!"
